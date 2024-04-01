25551f0e78d84ffc056547c5641482f71c291618
yudonggeun <ydong98@gmail.com>
2024-04-01 15:45:49 +0900

    first commit

---
+++ src/test/kotlin/com/sideproject/kping/KpingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sideproject.kping
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KpingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

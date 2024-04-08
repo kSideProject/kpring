aacfaa0b04def310c90d8291836bc16661417493
yudonggeun <ydong98@gmail.com>
2024-04-08 17:53:51 +0900

    chore : 테스트 라이브러리 추가
    - mockk
    - Spring Rest Docs

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -3,9 +3,15 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
+
+    // asciidoctor plugin
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
 
 dependencies {
+    // core module
+    api(project(":core"))
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -17,14 +23,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
-    // kotest
-    testImplementation("io.kotest:kotest-assertions-core")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
-
-    // default test
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-
     // jwt
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
@@ -33,6 +31,29 @@ dependencies {
     // dev tool
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    // core module
-    api(project(":core"))
+    // kotest
+    testImplementation("io.kotest:kotest-assertions-core")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
+    // mockk
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    // Spring rest docs
+    testImplementation("org.springframework.restdocs:spring-restdocs-webtestclient")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
+    // default test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+tasks.test {
+    outputs.dir(project.extra["snippetsDir"]!!)
+}
+
+tasks.asciidoctor {
+    inputs.dir(project.extra["snippetsDir"]!!)
+    dependsOn(tasks.test)
 }

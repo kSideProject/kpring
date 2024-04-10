d4badd9f63694b5dac42c09884157cae74e8149d
yudonggeun <ydong98@gmail.com>
2024-04-10 09:51:43 +0900

    refac : 토큰 생성 rest doc DSL 적용

--- auth/src/test/kotlin/kpring/auth/api/AuthControllerTest.kt
+++ auth/src/test/kotlin/kpring/auth/api/AuthControllerTest.kt
@@ -8,16 +8,14 @@ import kpring.auth.api.v1.AuthController
 import kpring.auth.service.TokenService
 import kpring.core.auth.dto.request.CreateTokenRequest
 import kpring.core.auth.dto.response.CreateTokenResponse
+import kpring.test.restdoc.dsl.restDoc
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.context.ApplicationContext
 import org.springframework.http.MediaType
 import org.springframework.restdocs.ManualRestDocumentation
-import org.springframework.restdocs.headers.HeaderDocumentation.*
 import org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint
-import org.springframework.restdocs.payload.PayloadDocumentation.*
-import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document
 import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration
 import org.springframework.test.web.reactive.server.WebTestClient
 import java.time.LocalDateTime
@@ -77,31 +75,32 @@ class AuthControllerTest(
                     .expectHeader().contentType(MediaType.APPLICATION_JSON)
                     .expectHeader().valuesMatch("Authorization", "Bearer .+")
                     .expectBody()
-                    .consumeWith(
-                        document(
-                            "post_api.v1.token",
-                            requestHeaders(
-                                headerWithName("Content-Type").description("application/json"),
-                            ),
-                            requestFields(
-                                fieldWithPath("id").description("유저 식별 아이디").type(String),
-                                fieldWithPath("nickname").description("닉네임").type(String)
-                            ),
+                    .restDoc("post_api.v1.token") {
+                        request {
+                            header {
+                                "Content-Type" mean "application/json"
+                            }
 
-                            responseHeaders(
-                                headerWithName("Authorization").description("jwt access token 정보"),
-                                headerWithName("Content-Type").description("application/json"),
-                            ),
-                            responseFields(
-                                fieldWithPath("accessToken").description("jwt access token").type(String),
-                                fieldWithPath("accessExpireAt").description("jwt access token 만료시간")
-                                    .type("yyyy-MM-dd hh:mm:ss"),
-                                fieldWithPath("refreshToken").description("jwt refresh token").type(String),
-                                fieldWithPath("refreshExpireAt").description("jwt refresh token 만료시간")
-                                    .type("yyyy-MM-dd hh:mm:ss"),
-                            ),
-                        ),
-                    )
+                            body {
+                                "id" type "String" mean "유저 식별 아이디"
+                                "nickname" type "String" mean "닉네임"
+                            }
+                        }
+
+                        response {
+                            header {
+                                "Authorization" mean "jwt access token 정보"
+                                "Content-Type" mean "application/json"
+                            }
+
+                            body {
+                                "accessToken" type "String" mean "jwt access token"
+                                "accessExpireAt" type "yyyy-MM-dd hh:mm:ss" mean "jwt access token 만료시간"
+                                "refreshToken" type "String" mean "jwt refresh token"
+                                "refreshExpireAt" type "yyyy-MM-dd hh:mm:ss" mean "jwt refresh token 만료시간"
+                            }
+                        }
+                    }
             }
 
             When("GET") {

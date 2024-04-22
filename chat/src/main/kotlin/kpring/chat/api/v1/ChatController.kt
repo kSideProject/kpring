f574a2362c5001b56f08d2348f01ab008daf6d05
minisun <minisundev@gmail.com>
2024-04-22 23:05:53 +0900

    feat: Chat Controller 에 createChat 메서드 구현

---
+++ chat/src/main/kotlin/kpring/chat/api/v1/ChatController.kt
@@ -0,0 +1,22 @@
+package kpring.chat.api.v1
+
+import kpring.chat.service.ChatService
+import kpring.core.chat.dto.request.CreateChatRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1")
+class ChatContoller (
+    private val chatService : ChatService
+){
+    @PostMapping("/chat")
+    fun createChat(
+        @Validated @RequestBody request: CreateChatRequest
+    ): ResponseEntity<*> {
+        var result = chatService.createChat(request)
+        return ResponseEntity.ok()
+            .body(result)
+    }
+}

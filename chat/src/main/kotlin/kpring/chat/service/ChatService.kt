5310489feb719427bd50ff9a4e2bc35b67861030
minisun <minisundev@gmail.com>
2024-04-24 22:11:01 +0900

    refac: createChat 성공시에 리턴되는 값 줄임

--- chat/src/main/kotlin/kpring/chat/service/ChatService.kt
+++ chat/src/main/kotlin/kpring/chat/service/ChatService.kt
@@ -3,7 +3,6 @@ package kpring.chat.service
 import kpring.chat.model.Chat
 import kpring.chat.repository.ChatRepository
 import kpring.core.chat.dto.request.CreateChatRequest
-import kpring.core.global.dto.response.ApiResponse
 import org.springframework.stereotype.Service
 
 @Service
@@ -15,15 +14,14 @@ class ChatService(
      */
     fun createChat(
         request: CreateChatRequest, userId: String
-    ): ApiResponse<Void> {
+    ): String {
 
         val chat = chatRepository.save(
             Chat(
                 userId = userId, roomId = request.room, nickname = request.nickname, content = request.content
             )
         )
-        return ApiResponse<Void>(
-            200, "success"
-        )
+
+        return "success"
     }
 }

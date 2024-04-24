a10f35919bddd236d4db063402144f3d945c5071
minisun <minisundev@gmail.com>
2024-04-24 20:58:02 +0900

    chore: gradle dependency 변경

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -21,11 +21,19 @@ repositories {
 dependencies {
     // core module
     api(project(":core"))
-    // auth module
-    api(project(":auth"))
 
+    // webflux
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // mongodb
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+    //web
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")

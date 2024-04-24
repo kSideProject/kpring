4d083406dbe729083c8510554f95cef97e7848e8
minisun <minisundev@gmail.com>
2024-04-24 21:03:26 +0900

    feat: createChat 함수에서 userId도 Chat model에 저장하도록 함

--- chat/src/main/kotlin/kpring/chat/service/ChatService.kt
+++ chat/src/main/kotlin/kpring/chat/service/ChatService.kt
@@ -1,9 +1,13 @@
 package kpring.chat.service
 
+import kpring.chat.exception.ErrorCode
+import kpring.chat.exception.GlobalException
 import kpring.chat.model.Chat
 import kpring.chat.repository.ChatRepository
+import kpring.core.auth.dto.response.TokenValidationResponse
 import kpring.core.chat.dto.request.CreateChatRequest
 import kpring.core.chat.dto.response.CreateChatResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,11 +17,15 @@ class ChatService(
     /*
      business logic
      */
-    fun createChat(request: CreateChatRequest): CreateChatResponse {
+    fun createChat(
+        request: CreateChatRequest,
+        tokenResponse: ResponseEntity<TokenValidationResponse>
+    ): CreateChatResponse {
 
+        val user = tokenResponse.body?.userId ?: throw GlobalException(ErrorCode.INVALID_TOKEN_BODY)
         val chat = chatRepository.save(
             Chat(
-                room = request.room, nickname = request.nickname, content = request.content
+                userId = user, roomId = request.room, nickname = request.nickname, content = request.content
             )
         )
         return CreateChatResponse(

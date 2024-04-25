37b5645c42cb55d08ca300910488d9361fe05229
minisun <minisundev@gmail.com>
2024-04-25 23:08:55 +0900

    feat: ChatRoomController에 createChatRoom 메서드 구현

---
+++ chat/src/main/kotlin/kpring/chat/chatroom/api/v1/ChatRoomController.kt
@@ -0,0 +1,29 @@
+package kpring.chat.chatroom.api.v1
+
+import kpring.chat.chatroom.service.ChatRoomService
+import kpring.chat.global.exception.ErrorCode
+import kpring.chat.global.exception.GlobalException
+import kpring.core.auth.client.AuthClient
+import kpring.core.chat.chatroom.dto.request.CreateChatRoomRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1")
+class ChatRoomController (
+    private val chatRoomService: ChatRoomService,
+    private val authClient: AuthClient
+){
+    @PostMapping("/chatroom")
+    fun createChatRoom(
+        @Validated @RequestBody request: CreateChatRoomRequest,
+        @RequestHeader("Authorization") token: String
+    ): ResponseEntity<*> {
+        val tokenResponse = authClient.validateToken(token)
+        val userId = tokenResponse.body?.userId ?: throw GlobalException(ErrorCode.INVALID_TOKEN_BODY)
+
+        val result = chatRoomService.createChatRoom(request, userId)
+        return ResponseEntity.ok().body(result)
+    }
+}

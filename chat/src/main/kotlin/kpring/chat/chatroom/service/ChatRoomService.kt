e27ea06078cc918d4d8916e4320945295911d10f
minisun <minisundev@gmail.com>
2024-04-25 23:09:06 +0900

    feat: ChatRoomService에 createChatRoom 메서드 구현

---
+++ chat/src/main/kotlin/kpring/chat/chatroom/service/ChatRoomService.kt
@@ -0,0 +1,21 @@
+package kpring.chat.chatroom.service
+
+import kpring.chat.chatroom.model.ChatRoom
+import kpring.chat.chatroom.repository.ChatRoomRepository
+import kpring.core.chat.chatroom.dto.request.CreateChatRoomRequest
+import org.springframework.stereotype.Service
+
+@Service
+class ChatRoomService(
+    private val chatRoomRepository: ChatRoomRepository
+) {
+    fun createChatRoom(
+        request: CreateChatRoomRequest,
+        userId: String
+    ): String {
+
+        val chatRoom = chatRoomRepository.save(ChatRoom(request.users))
+
+        return "success"
+    }
+}

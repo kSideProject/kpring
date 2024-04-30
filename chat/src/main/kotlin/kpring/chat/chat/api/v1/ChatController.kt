54c1d69e49678542bdcc6c0b04bc1d0ed955710e
minisun <minisundev@gmail.com>
2024-04-30 23:32:30 +0900

    refac: token이 유효한지 체크하는 공통 로직 메서드로 분리

--- chat/src/main/kotlin/kpring/chat/chat/api/v1/ChatController.kt
+++ chat/src/main/kotlin/kpring/chat/chat/api/v1/ChatController.kt
@@ -4,6 +4,7 @@ import kpring.chat.global.exception.ErrorCode
 import kpring.chat.global.exception.GlobalException
 import kpring.chat.chat.service.ChatService
 import kpring.core.auth.client.AuthClient
+import kpring.core.auth.dto.response.TokenValidationResponse
 import kpring.core.chat.chat.dto.request.CreateChatRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
@@ -11,23 +12,26 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1")
-class ChatContoller(
+class ChatController(
     private val chatService: ChatService, val authClient: AuthClient
 ) {
 
     @PostMapping("/chat")
     fun createChat(
-        @Validated @RequestBody request: CreateChatRequest, @RequestHeader("Authorization") token: String
+        @Validated @RequestBody request: CreateChatRequest,
+        @RequestHeader("Authorization") token: String
     ): ResponseEntity<*> {
-        val tokenResponse = authClient.validateToken(token)
-        var body = tokenResponse.body?: throw GlobalException(ErrorCode.INVALID_TOKEN_BODY)
-        val userId = body.userId?: throw GlobalException(ErrorCode.USERID_NOT_EXIST)
-        var isValid = body.isValid
-        if(!isValid){
-            throw GlobalException(ErrorCode.INVALID_TOKEN)
-        }
-
+        val userId = validateToken(authClient.validateToken(token));
         val result = chatService.createChat(request, userId)
         return ResponseEntity.ok().body(result)
+    }
+
+    private fun validateToken(tokenResponse :  ResponseEntity<TokenValidationResponse>) : String
+    {
+        val body = tokenResponse.body?: throw GlobalException(ErrorCode.INVALID_TOKEN_BODY)
+        val userId = body.userId?: throw GlobalException(ErrorCode.USERID_NOT_EXIST)
+        if(!body.isValid){ throw GlobalException(ErrorCode.INVALID_TOKEN) }
+
+        return userId;
     }
 }

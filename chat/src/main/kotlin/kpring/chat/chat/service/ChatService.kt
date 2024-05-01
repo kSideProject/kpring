3c92cffc5afacb36afc5bcf9862341039e976d88
minisun <minisundev@gmail.com>
2024-05-01 20:37:59 +0900

    feat: 채팅방 별 채팅 조회 메서드 Service단 구현

--- chat/src/main/kotlin/kpring/chat/chat/service/ChatService.kt
+++ chat/src/main/kotlin/kpring/chat/chat/service/ChatService.kt
@@ -2,12 +2,19 @@ package kpring.chat.chat.service
 
 import kpring.chat.chat.model.Chat
 import kpring.chat.chat.repository.ChatRepository
+import kpring.chat.chatroom.model.ChatRoom
+import kpring.chat.chatroom.repository.ChatRoomRepository
+import kpring.chat.global.exception.ErrorCode
+import kpring.chat.global.exception.GlobalException
 import kpring.core.chat.chat.dto.request.CreateChatRequest
+import kpring.core.chat.chat.dto.response.ChatResponse
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class ChatService(
-    private val chatRepository: ChatRepository
+    private val chatRepository: ChatRepository,
+    private val chatRoomRepository: ChatRoomRepository
 ) {
     /*
      business logic
@@ -21,5 +28,28 @@ class ChatService(
                 userId = userId, roomId = request.room, content = request.content
             )
         )
+    }
+
+    fun getChatsByChatRoom(
+        chatRoomId: String, userId: String
+    ) : List<ChatResponse>{
+
+        checkIfUserBelongToChatRoom(chatRoomId, userId)
+        val chats : List<Chat> = chatRepository.findAllByRoomId(chatRoomId)
+        val chatResponses = chats.map { chat ->
+            ChatResponse(chat.roomId, chat.isDeleted, chat.isEdited, chat.sentAt, chat.content)
+        }
+
+        return chatResponses
+    }
+
+    fun checkIfUserBelongToChatRoom(chatRoomId: String, userId: String) {
+        val chatroom : ChatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow{GlobalException(ErrorCode.CHATROOM_NOT_FOUND)}
+        for(member in chatroom.getUsers()){
+            if(member.equals(userId)){
+                return;
+            }
+        }
+        throw GlobalException(ErrorCode.UNAUTHORIZED_CHATROOM)
     }
 }

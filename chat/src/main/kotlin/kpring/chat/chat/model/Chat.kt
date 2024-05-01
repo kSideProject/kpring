18054703dee0520338715da15b975e7e47599e42
minisun <minisundev@gmail.com>
2024-05-01 20:31:33 +0900

    feat: Chat model에 isEdited 필드 추가

--- chat/src/main/kotlin/kpring/chat/chat/model/Chat.kt
+++ chat/src/main/kotlin/kpring/chat/chat/model/Chat.kt
@@ -1,23 +1,29 @@
 package kpring.chat.chat.model
 
+import lombok.Getter
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 
+@Getter
 @Document(collection = "chats")
 class Chat(
     val userId: String,
     val roomId: String,
-    val content: String
+    val content: String,
+    val sentAt: LocalDateTime = LocalDateTime.now()
 ) {
     @Id
     var id: String? = null
 
     var isDeleted: Boolean = false
+    var isEdited: Boolean = false
 
-    val sentAt: LocalDateTime = LocalDateTime.now()
-
-    fun deleted() {
+    fun delete() {
         isDeleted = true
+    }
+
+    fun edit(){
+        isEdited = true;
     }
 }

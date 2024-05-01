cdfde5b02494c718ef8335ea7768d3b806e601d1
minahYu <mina010311@naver.com>
2024-05-01 17:47:26 +0900

    chore: Spring Security 자동 구성 비활성화

--- user/src/main/kotlin/kpring/user/UserApplication.kt
+++ user/src/main/kotlin/kpring/user/UserApplication.kt
@@ -1,9 +1,10 @@
 package kpring.user
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class UserApplication
 
 fun main(args: Array<String>) {

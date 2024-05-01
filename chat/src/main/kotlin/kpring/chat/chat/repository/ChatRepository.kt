289b008739602a776d96667c7b137877e8d0045c
minisun <minisundev@gmail.com>
2024-05-01 20:38:50 +0900

    feat: findAllByRoomId 메서드 정의

--- chat/src/main/kotlin/kpring/chat/chat/repository/ChatRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chat/repository/ChatRepository.kt
@@ -3,7 +3,9 @@ package kpring.chat.chat.repository
 import kpring.chat.chat.model.Chat
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface ChatRepository : MongoRepository<Chat,String> {
+    fun findAllByRoomId(roomId : String): List<Chat>
 }

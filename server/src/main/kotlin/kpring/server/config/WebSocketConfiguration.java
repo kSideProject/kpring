80b7db7a128be4b497e90cd69be948463fae906b
임지훈 <lonelynight1026@gmail.com>
2024-05-02 19:17:29 +0900

    remove : WebSocketConfiguration > 주석처리한 이전 코드 제거

--- server/src/main/kotlin/kpring/server/config/WebSocketConfiguration.java
+++ server/src/main/kotlin/kpring/server/config/WebSocketConfiguration.java
@@ -23,22 +23,3 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
                 .withSockJS(); // 브라우저에서 WebSocket 을 지원하지 않는 경우에 대안으로 어플리케이션의 코드를 변경할 필요 없이 런타임에 필요할 때 대체하기 위해 설정
     }
 }
-
-/**
- * [Baeldung Guide to WebRTC](https://www.baeldung.com/webrtc)
- */
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.socket.config.annotation.EnableWebSocket;
-//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-//
-//@Configuration
-//@EnableWebSocket
-//public class WebSocketConfiguration implements WebSocketConfigurer {
-//
-//    @Override
-//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(new SocketHandler(), "/socket")
-//                .setAllowedOrigins("*");
-//    }
-//}

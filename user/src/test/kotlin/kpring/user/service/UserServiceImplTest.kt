a0652397ffa3cd4b4586ba759ba19340f33c37a7
minahYu <mina010311@naver.com>
2024-05-04 02:59:20 +0900

    test: UserServiceImpl 비밀번호 불일치로 인한 회원가입 실패 테스트 작성

--- user/src/test/kotlin/kpring/user/service/UserServiceImplTest.kt
+++ user/src/test/kotlin/kpring/user/service/UserServiceImplTest.kt
@@ -82,6 +82,32 @@ class UserServiceImplTest() : FunSpec({
         verify { userRepository.save(any()) wasNot Called }
     }
 
+    test("회원가입_실패_비밀번호불일치케이스") {
+        createUserRequest = CreateUserRequest(
+            TEST_EMAIL,
+            TEST_PASSWORD,
+            MISMATCHED_PASSWORD,
+            TEST_USERNAME
+        )
+
+        every {
+            userValidationService.validatePasswordMatch(
+                createUserRequest.password,
+                createUserRequest.passwordCheck
+            )
+        } throws ExceptionWrapper(ErrorCode.NOT_MATCH_PASSWORD)
+
+        val exception = shouldThrow<ExceptionWrapper> {
+            userValidationService.validatePasswordMatch(
+                createUserRequest.password,
+                createUserRequest.passwordCheck
+            )
+        }
+        exception.errorCode.message shouldBe "Password does not match"
+
+        verify { userRepository.save(any()) wasNot Called }
+    }
+
 //    test("친구추가_성공") {
 //        val user = User(id = 1L, username = "user1", followers = mutableSetOf(), followees = mutableSetOf())
 //        val friend = User(id = 2L, username = "user2", followers = mutableSetOf(), followees = mutableSetOf())

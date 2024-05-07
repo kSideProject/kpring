161e87b9da67a1c371356d97e23ab91a2c86c872
minisun <minisundev@gmail.com>
2024-05-07 22:06:32 +0900

    feat: ChatServiceTest에 getChatsByChatRoom 테스트코드 작성

--- chat/src/test/kotlin/kpring/chat/chat/ChatServiceTest.kt
+++ chat/src/test/kotlin/kpring/chat/chat/ChatServiceTest.kt
@@ -42,5 +42,40 @@ class ChatServiceTest(
         verify { chatRepository.save(any()) }
     }
 
+    test("getChatsByChatRoom 은 권한이 없는 사용자에게 에러 발생") {
+        // Given
+        val chatRoomId = ChatRoomTest.TEST_ROOM_ID
+        val userId = CommonTest.TEST_ANOTHER_USER_ID
+        every { chatRoomRepository.existsByIdAndMembersContaining(chatRoomId, userId) } returns false
+
+        // When & Then
+        val exception = shouldThrow<GlobalException> {
+            chatService.getChatsByChatRoom(chatRoomId, userId, 1)
+        }
+        val errorCodeField = GlobalException::class.java.getDeclaredField("errorCode")
+        errorCodeField.isAccessible = true
+        val errorCode = errorCodeField.get(exception) as ErrorCode
+        errorCode shouldBe ErrorCode.UNAUTHORIZED_CHATROOM
+    }
+
+    test("getChatsByChatRoom 은 권한이 있는 사용자에게 Chat들을 반환") {
+        // Given
+        val chatRoomId = ChatRoomTest.TEST_ROOM_ID
+        val userId = CommonTest.TEST_USER_ID
+        val chat1 = Chat(userId, chatRoomId, "Message 1")
+        val chat2 = Chat(userId, chatRoomId, "Message 2")
+        every { chatRepository.findAllByRoomId(chatRoomId, pageable = PageRequest.of(1, pageSize)) } returns listOf(
+            chat1, chat2
+        )
+        every { chatRoomRepository.existsByIdAndMembersContaining(chatRoomId, userId) } returns true
+
+        // When
+        val chatResponses = chatService.getChatsByChatRoom(chatRoomId, userId, 1)
+
+        // Then
+        chatResponses.size shouldBe 2
+        chatResponses.shouldContain(ChatResponse(chatRoomId, false, chat1.createdAt, "Message 1"))
+        chatResponses.shouldContain(ChatResponse(chatRoomId, false, chat2.createdAt, "Message 2"))
+    }
 
 })

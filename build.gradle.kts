d92ccd81fa62134d82bfc8fe98fab277bab7b112
yudonggeun <ydong98@gmail.com>
2024-05-07 17:17:34 +0900

    chore : query dsl mongo 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
+    kotlin("kapt") version "1.9.23"
 }
 
 allprojects {

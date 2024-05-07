bc0db2abea6c9e117359c33251a38c2200127925
minisun <minisundev@gmail.com>
2024-05-07 21:22:16 +0900

    refac: findAllByRoomId 메서드에 Pagination 적용

--- chat/src/main/kotlin/kpring/chat/chat/repository/ChatRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chat/repository/ChatRepository.kt
@@ -1,11 +1,12 @@
 package kpring.chat.chat.repository
 
 import kpring.chat.chat.model.Chat
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ChatRepository : MongoRepository<Chat, String> {
-    fun findAllByRoomId(roomId: String): List<Chat>
+    fun findAllByRoomId(roomId: String, pageable: Pageable): List<Chat>
 
 }

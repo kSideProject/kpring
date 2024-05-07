c5e2fb43d9340d3b5844271b0c5c674a7b3b6168
yudonggeun <ydong98@gmail.com>
2024-05-07 11:36:54 +0900

    chore : 테스트 의존성 추가
    - kotest & mockk
    - spring rest docs
    - custom test module

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -22,9 +22,6 @@ dependencies {
     // core module
     api(project(":core"))
 
-    // webflux
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-
     // mongodb
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
@@ -36,7 +33,21 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // kotest
+    testImplementation("io.kotest:kotest-assertions-core")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
+    // mockk
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    // Spring rest docs
+    testImplementation("org.springframework.restdocs:spring-restdocs-webtestclient")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
+    // default test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(project(":test"))
 }
 
 tasks.withType<KotlinCompile> {

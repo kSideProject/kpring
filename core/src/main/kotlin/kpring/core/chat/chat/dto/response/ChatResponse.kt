d224fecdac6bffabe5b240ef211db65ed7b480be
minisun <minisundev@gmail.com>
2024-05-07 21:57:40 +0900

    refac: isDeleted 삭제

--- core/src/main/kotlin/kpring/core/chat/chat/dto/response/ChatResponse.kt
+++ core/src/main/kotlin/kpring/core/chat/chat/dto/response/ChatResponse.kt
@@ -4,7 +4,6 @@ import java.time.LocalDateTime
 
 data class ChatResponse(
     val id: String,
-    val isDeleted: Boolean,
     val isEdited: Boolean,
     val sentAt: LocalDateTime,
     val content: String

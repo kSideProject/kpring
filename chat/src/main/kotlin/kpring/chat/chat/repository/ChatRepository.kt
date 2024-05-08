8fdbb80f2a1e55ade67ee967850648cbd0ae1a29
minisun <minisundev@gmail.com>
2024-05-08 23:37:43 +0900

    style: 코드 정렬

--- chat/src/main/kotlin/kpring/chat/chat/repository/ChatRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chat/repository/ChatRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ChatRepository : MongoRepository<Chat,String>, QuerydslPredicateExecutor<Chat>{
-      fun findAllByRoomId(roomId: String, pageable: Pageable): List<Chat>
+interface ChatRepository : MongoRepository<Chat, String>, QuerydslPredicateExecutor<Chat> {
+
+    fun findAllByRoomId(roomId: String, pageable: Pageable): List<Chat>
 }

d24bb96f7506dd1ef2ce291ce091fd837d5819c6
minisun <minisundev@gmail.com>
2024-05-08 16:54:28 +0900

    refac: createChatRoom 메서드에서 token에서 userId를 가져오며 검증하는 메서드 분리

--- chat/src/main/kotlin/kpring/chat/chatroom/api/v1/ChatRoomController.kt
+++ chat/src/main/kotlin/kpring/chat/chatroom/api/v1/ChatRoomController.kt
@@ -4,6 +4,7 @@ import kpring.chat.chatroom.service.ChatRoomService
 import kpring.chat.global.exception.ErrorCode
 import kpring.chat.global.exception.GlobalException
 import kpring.core.auth.client.AuthClient
+import kpring.core.auth.dto.response.TokenValidationResponse
 import kpring.core.chat.chatroom.dto.request.CreateChatRoomRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
@@ -18,14 +19,20 @@ class ChatRoomController(
     fun createChatRoom(
         @Validated @RequestBody request: CreateChatRoomRequest, @RequestHeader("Authorization") token: String
     ): ResponseEntity<*> {
-        val tokenResponse = authClient.validateToken(token)
-        val body = tokenResponse.body ?: throw GlobalException(ErrorCode.INVALID_TOKEN_BODY)
-        val userId = body.userId ?: throw GlobalException(ErrorCode.USERID_NOT_EXIST)
-        val isValid = body.isValid
-        if (!isValid) {
-            throw GlobalException(ErrorCode.INVALID_TOKEN)
-        }
+
+        val userId = getUserId(authClient.validateToken(token))
+
         val result = chatRoomService.createChatRoom(request, userId)
         return ResponseEntity.ok().body(result)
+    }
+
+    private fun getUserId(tokenResponse: ResponseEntity<TokenValidationResponse>): String {
+        val body = tokenResponse.body ?: throw GlobalException(ErrorCode.INVALID_TOKEN_BODY)
+        val userId = body.userId ?: throw GlobalException(ErrorCode.USERID_NOT_EXIST)
+        if (!body.isValid) {
+            throw GlobalException(ErrorCode.INVALID_TOKEN)
+        }
+
+        return userId
     }
 }

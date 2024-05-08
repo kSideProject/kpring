83cae70706e23b78d55e0735fc22f9645beba3a9
minisun <minisundev@gmail.com>
2024-05-08 23:36:53 +0900

    fix: 어노테이션 추가, 기본 생성자 추가

--- chat/src/main/kotlin/kpring/chat/chat/model/Chat.kt
+++ chat/src/main/kotlin/kpring/chat/chat/model/Chat.kt
@@ -1,20 +1,24 @@
 package kpring.chat.chat.model
 
+import jakarta.persistence.Entity
 import kpring.chat.global.model.BaseTime
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-
+@Entity
 @Document(collection = "chats")
 class Chat(
     val userId: String,
     val roomId: String,
     val content: String,
-) : BaseTime() {
-    @Id
-    var id: String? = null
+) : BaseTime(){
+
+    @Id()
+    var id: String ?= null
 
     fun isEdited(): Boolean {
         return !createdAt.equals(updatedAt)
     }
+
+    constructor() : this("", "", "")
 
 }

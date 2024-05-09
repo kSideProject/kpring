cf91a55fccfcae18ed1794e7d340030868c306a9
minisun <minisundev@gmail.com>
2024-05-09 15:54:47 +0900

    chore: noArg 플러그인 추가

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -6,30 +6,29 @@ plugins {
   kotlin("jvm") version "1.9.23"
   kotlin("plugin.spring") version "1.9.23"
   kotlin("kapt") version "1.9.23"
+  kotlin("plugin.noarg") version "1.9.24"
 }
 
+noArg {
+  annotation("com.my.Annotation")
+  invokeInitializers = true
+}
+
+group = "kpring"
+version = "0.0.1-SNAPSHOT"
 val queryDslVersion = "5.1.0"
 
-dependencies {
+java {
+  sourceCompatibility = JavaVersion.VERSION_21
+}
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
+repositories {
+  mavenCentral()
+}
+
+dependencies {
   // core module
   api(project(":core"))
-
-  // mongodb
-  implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-  implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-  implementation("com.querydsl:querydsl-mongodb:$queryDslVersion") {
-    exclude("org.mongodb", "mongo-java-driver")
-  }
-  implementation("com.querydsl:querydsl-jpa:$queryDslVersion")
-  kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
-  // web
-  implementation("org.springframework.boot:spring-boot-starter-web")
-
-  // validation
-  implementation("org.springframework.boot:spring-boot-starter-validation")
 
   // test
   testImplementation(project(":test"))
@@ -51,7 +50,38 @@ dependencies {
   implementation("org.springframework.restdocs:spring-restdocs-webtestclient")
   implementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
 
+  // mongodb
+  implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+  implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+  implementation("com.querydsl:querydsl-mongodb:$queryDslVersion") {
+    exclude("org.mongodb", "mongo-java-driver")
+  }
+  implementation("com.querydsl:querydsl-jpa:$queryDslVersion")
+  kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
+
+  // web
+  implementation("org.springframework.boot:spring-boot-starter-web")
+
+  // validation
+  implementation("org.springframework.boot:spring-boot-starter-validation")
+
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+  // kotest
+  testImplementation("io.kotest:kotest-assertions-core")
+  testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
+  // mockk
+  testImplementation("io.mockk:mockk:1.13.10")
+  testImplementation("com.ninja-squad:springmockk:4.0.2")
+  // Spring rest docs
+  testImplementation("org.springframework.restdocs:spring-restdocs-webtestclient")
+  testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+  testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
+  // default test
   testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation(project(":test"))
 }
 
 tasks.withType<KotlinCompile> {

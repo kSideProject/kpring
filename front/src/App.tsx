c3159070e065c9bc0280c33e52025bdbf6da440b
jihyun-j <jihyun9142@gmail.com>
2024-05-13 16:39:28 +0900

    [fear]Phaser.js 설치 및 테스트 배경 설정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./App.css";
 import Layout from "./components/Layout/Layout";
-
+import { PhaserGame } from "./components/Phaser/PhaserGame";
 function App() {
   return (
     <div className="App">
       <Layout>
         <main>dididiidisjdsjfkldsjl</main>
+        <PhaserGame />
       </Layout>
     </div>
   );
---
+++ front/src/components/Phaser/scenes/Boot.ts
@@ -0,0 +1,20 @@
+import { Scene } from "phaser";
+
+export class Boot extends Scene {
+  constructor() {
+    super("Boot");
+  }
+
+  preload() {
+    //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
+    //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
+    this.load.image(
+      "background",
+      "https://images.unsplash.com/photo-1553481187-be93c21490a9?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    );
+  }
+
+  create() {
+    this.scene.start("Preloader");
+  }
+}

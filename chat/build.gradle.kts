3f12768057640c8acd3d8e90023a9682a2009efb
minisun <minisundev@gmail.com>
2024-05-13 18:05:14 +0900

    feat: NoArg annotation 관련 설정

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 noArg {
-  annotation("com.my.Annotation")
+  annotation("kpring.chat.NoArg")
   invokeInitializers = true
 }
 
@@ -29,6 +29,24 @@ repositories {
 dependencies {
   // core module
   api(project(":core"))
+
+  // mongodb
+  implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+  implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+  implementation("com.querydsl:querydsl-mongodb:$queryDslVersion") {
+    exclude("org.mongodb", "mongo-java-driver")
+  }
+  implementation("com.querydsl:querydsl-jpa:$queryDslVersion")
+  kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
+
+  // web
+  implementation("org.springframework.boot:spring-boot-starter-web")
+
+  // validation
+  implementation("org.springframework.boot:spring-boot-starter-validation")
+
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
 
   // test
   testImplementation(project(":test"))
@@ -49,35 +67,6 @@ dependencies {
   implementation("org.springframework.restdocs:spring-restdocs-mockmvc")
   implementation("org.springframework.restdocs:spring-restdocs-webtestclient")
   implementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
-
-  // mongodb
-  implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-  implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-  implementation("com.querydsl:querydsl-mongodb:$queryDslVersion") {
-    exclude("org.mongodb", "mongo-java-driver")
-  }
-  implementation("com.querydsl:querydsl-jpa:$queryDslVersion")
-  kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
-
-  // web
-  implementation("org.springframework.boot:spring-boot-starter-web")
-
-  // validation
-  implementation("org.springframework.boot:spring-boot-starter-validation")
-
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-  // kotest
-  testImplementation("io.kotest:kotest-assertions-core")
-  testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
-  // mockk
-  testImplementation("io.mockk:mockk:1.13.10")
-  testImplementation("com.ninja-squad:springmockk:4.0.2")
-  // Spring rest docs
-  testImplementation("org.springframework.restdocs:spring-restdocs-webtestclient")
-  testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
-  testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
 
   // default test
   testImplementation("org.springframework.boot:spring-boot-starter-test")

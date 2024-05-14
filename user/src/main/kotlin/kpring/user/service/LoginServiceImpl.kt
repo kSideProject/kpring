a8191cea9de542e3f8f61ede0e1451b743d8cda9
minahYu <mina010311@naver.com>
2024-05-14 15:33:34 +0900

    refac: userValidationService 역할 구체화를 위한 코드 수정

--- user/src/main/kotlin/kpring/user/service/LoginServiceImpl.kt
+++ user/src/main/kotlin/kpring/user/service/LoginServiceImpl.kt
@@ -2,27 +2,39 @@ package kpring.user.service
 
 import kpring.core.auth.client.AuthClient
 import kpring.core.auth.dto.request.CreateTokenRequest
+import kpring.core.auth.dto.response.CreateTokenResponse
 import kpring.user.dto.request.LoginRequest
 import kpring.user.dto.request.LogoutRequest
 import kpring.user.dto.response.LoginResponse
+import kpring.user.exception.ErrorCode
+import kpring.user.exception.ExceptionWrapper
+import kpring.user.repository.UserRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
 class LoginServiceImpl(
+  private val userRepository: UserRepository,
   private val authClient: AuthClient,
   private val userValidationService: UserValidationService,
 ) : LoginService {
   override fun login(request: LoginRequest): LoginResponse {
-    val user = userValidationService.validateExistEmail(request.email)
+    val user =
+      userRepository.findByEmail(request.email) ?: throw ExceptionWrapper(ErrorCode.USER_NOT_FOUND)
     userValidationService.validateUserPassword(request.password, user.password)
 
     val createTokenRequest = CreateTokenRequest((user.id).toString(), user.username)
     val tokenResponse = authClient.createToken(createTokenRequest)
 
-    return userValidationService.validateTokenResponse(tokenResponse)
+    return handleTokenResponse(tokenResponse)
   }
 
   override fun logout(request: LogoutRequest) {
     TODO("Not yet implemented")
+  }
+
+  fun handleTokenResponse(tokenResponse: ResponseEntity<CreateTokenResponse>): LoginResponse {
+    val body = tokenResponse.body ?: throw ExceptionWrapper(ErrorCode.NOT_ALLOWED)
+    return LoginResponse(body.accessToken, body.refreshToken)
   }
 }
--- user/src/main/kotlin/kpring/user/service/UserValidationService.kt
+++ user/src/main/kotlin/kpring/user/service/UserValidationService.kt
@@ -1,26 +1,14 @@
 package kpring.user.service
 
-import kpring.core.auth.dto.response.CreateTokenResponse
-import kpring.user.dto.response.LoginResponse
-import kpring.user.entity.User
 import kpring.user.exception.ErrorCode
 import kpring.user.exception.ExceptionWrapper
-import kpring.user.repository.UserRepository
-import org.springframework.http.ResponseEntity
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserValidationService(
-  private val userRepository: UserRepository,
   private val passwordEncoder: PasswordEncoder,
 ) {
-  fun validateDuplicateEmail(email: String) {
-    if (userRepository.existsByEmail(email)) {
-      throw ExceptionWrapper(ErrorCode.ALREADY_EXISTS_EMAIL)
-    }
-  }
-
   fun validatePasswordMatch(
     password: String,
     passwordCheck: String,
@@ -30,10 +18,6 @@ class UserValidationService(
     }
   }
 
-  fun validateExistEmail(email: String): User {
-    return userRepository.findByEmail(email) ?: throw ExceptionWrapper(ErrorCode.USER_NOT_FOUND)
-  }
-
   fun validateUserPassword(
     rawPassword: String,
     encodedPassword: String,
@@ -41,10 +25,5 @@ class UserValidationService(
     if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
       throw ExceptionWrapper(ErrorCode.INCORRECT_PASSWORD)
     }
-  }
-
-  fun validateTokenResponse(tokenResponse: ResponseEntity<CreateTokenResponse>): LoginResponse {
-    val body = tokenResponse.body ?: throw ExceptionWrapper(ErrorCode.NOT_ALLOWED)
-    return LoginResponse(body.accessToken, body.refreshToken)
   }
 }

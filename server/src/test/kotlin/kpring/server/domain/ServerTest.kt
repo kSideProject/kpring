ce62378c9abf8c77be8ca80fc170260f85bd2a48
yudonggeun <ydong98@gmail.com>
2024-05-14 17:22:07 +0900

    test : 서버 도메인 메서드 테스트

---
+++ server/src/test/kotlin/kpring/server/domain/ServerTest.kt
@@ -0,0 +1,50 @@
+package kpring.server.domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldNotContain
+import io.kotest.matchers.shouldBe
+import kpring.core.global.exception.ServiceException
+import kpring.server.error.ServerErrorCode
+
+/**
+ * Server 도메인 단위 테스트입니다.
+ */
+class ServerTest : DescribeSpec({
+
+  it("초대된 유저라면 서버에 유저가 가입할 때, 초대 목록에서 제거하고 가입 목록에 추가한다.") {
+    // given
+    val server = Server(
+      "serverId", "serverName", mutableSetOf(),
+      invitedUserIds = mutableSetOf("invitedUserId")
+    )
+    val user = ServerUser("invitedUserId", "userName", "profileImageUrl")
+
+    // when
+    server.addUser(user)
+
+    // then
+    server.invitedUserIds shouldNotContain user.id
+    server.users shouldContain user
+  }
+
+  it("이미 등록된 유저를 초대시 예외가 발생한다.") {
+    // given
+    val server = Server(
+      "serverId", "serverName", mutableSetOf(),
+      invitedUserIds = mutableSetOf("invitedUserId")
+    )
+    val user = ServerUser("invitedUserId", "userName", "profileImageUrl")
+    server.addUser(user)
+
+    // when
+    val result = shouldThrow<ServiceException> {
+      server.registerInvitation(user.id)
+    }.errorCode
+
+    // then
+    result shouldBe ServerErrorCode.ALREADY_REGISTERED_USER
+  }
+})
+

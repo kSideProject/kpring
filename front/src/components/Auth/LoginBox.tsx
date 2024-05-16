f6bc33874d47e7aadbe7a4e4ce5e5d2a798b2f83
allone9425 <allone9425@gmail.com>
2024-05-16 17:04:15 +0900

    [feat] 로그인 페이지 UI 렌더링

---
+++ front/src/components/Auth/LoginBox.tsx
@@ -0,0 +1,64 @@
+import LoginIcon from "@mui/icons-material/Login";
+import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+function LoginBox() {
+  return (
+    <section className="flex justify-center mt-[200px]">
+      <div className="mt-[30px] w-[400px] text-center">
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { mb: 3, width: "300px" },
+          }}
+          noValidate
+          autoComplete="off"
+          bgcolor="#FDE2F3"
+          borderRadius="10px"
+          padding="20px"
+        >
+          <h2 className="text-center text-primary">디코타운에 어서오세요!</h2>
+          <TextField
+            required
+            id="user-email"
+            label="이메일"
+            placeholder="이메일을 입력해주세요."
+            variant="standard"
+            size="small"
+          />
+          <TextField
+            required
+            id="user-password"
+            label="비밀번호"
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            autoComplete="current-password"
+            variant="standard"
+            size="small"
+          />
+          <div className="mt-[20px] flex justify-center flex-wrap ">
+            <Button
+              variant="contained"
+              startIcon={<LoginIcon />}
+              sx={{ width: "90%" }}
+            >
+              로그인
+            </Button>
+
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<PersonAddAlt1Icon />}
+              sx={{ mt: "20px", width: "90%", mb: "20px" }}
+            >
+              회원가입
+            </Button>
+          </div>
+        </Box>
+      </div>
+    </section>
+  );
+}
+
+export default LoginBox;
--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,5 +1,29 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import LoginBox from "../components/Auth/LoginBox";
 const Login = () => {
-  return <div>로그인</div>;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#FDE2F3",
+        main: "#917FB3",
+        dark: "#E5BEEC",
+        contrastText: "#fff",
+      },
+      secondary: {
+        light: "#2A2F4F",
+        main: "#2A2F4F",
+        dark: "#E5BEEC",
+        contrastText: "#fff",
+      },
+    },
+  });
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <LoginBox />
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default Login;

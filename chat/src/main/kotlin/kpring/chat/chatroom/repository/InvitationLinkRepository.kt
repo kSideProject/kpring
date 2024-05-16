a55e311e1574b1c586e69aed0b3322b51d761512
minisun <minisundev@gmail.com>
2024-05-16 21:39:27 +0900

    feat: 채팅방 초대 링크 생성 메서드 Repository layer 구현

---
+++ chat/src/main/kotlin/kpring/chat/chatroom/repository/InvitationLinkRepository.kt
@@ -0,0 +1,35 @@
+package kpring.chat.chatroom.repository
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+import java.time.Duration
+import java.util.*
+
+@Component
+class InvitationLinkRepository(
+  private val redisTemplate: RedisTemplate<String, String>
+) {
+  @Value("\${invitation.expiration}")
+  private val expiration: Long = 3600
+
+  fun saveChatRoomIdAndGetLink(chatRoomId: String): String {
+    val uniqueId = UUID.randomUUID().toString()
+    val key = generateUniqueKey(uniqueId)
+    set(key, chatRoomId, Duration.ofSeconds(expiration))
+    return generateAddress(uniqueId)
+  }
+
+  private fun generateAddress(uniqueId: String): String {
+    return "//$uniqueId"//후에 참여 API나오면 주소로 대체할 예정
+  }
+
+  private fun generateUniqueKey(uniqueId: String): String {
+    return "invitation:$uniqueId"
+  }
+
+  private fun set(key: String, value: String, duration: Duration) {
+    redisTemplate.opsForValue().set(key, value, duration.seconds, java.util.concurrent.TimeUnit.SECONDS)
+  }
+
+}

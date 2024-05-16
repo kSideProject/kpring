ae76f7500025d74e59373e069133e4cba0526f7e
minisun <minisundev@gmail.com>
2024-05-16 16:08:06 +0900

    refac: members 를 List에서 Set으로 변경하여 User가 중복 저장되지 않도록 함

--- chat/src/main/kotlin/kpring/chat/chatroom/model/ChatRoom.kt
+++ chat/src/main/kotlin/kpring/chat/chatroom/model/ChatRoom.kt
@@ -3,16 +3,17 @@ package kpring.chat.chatroom.model
 import kpring.chat.global.model.BaseTime
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.transaction.annotation.Transactional
 
 @Document(collection = "chatrooms")
 class ChatRoom : BaseTime() {
   @Id
   var id: String? = null
 
-  var members: MutableList<String> = mutableListOf()
+  var members: MutableSet<String> = mutableSetOf()
 
   fun getUsers(): List<String> {
-    return members
+    return members.stream().toList()
   }
 
   fun addUsers(list: List<String>) {

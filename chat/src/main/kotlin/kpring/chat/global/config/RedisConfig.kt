d71a8d5b9c317fb3e1489eb8dd90df277260066e
minisun <minisundev@gmail.com>
2024-05-17 15:01:39 +0900

    feat: redisTemplate Bean 메서드 생성

--- chat/src/main/kotlin/kpring/chat/global/config/RedisConfig.kt
+++ chat/src/main/kotlin/kpring/chat/global/config/RedisConfig.kt
@@ -1,7 +1,17 @@
 package kpring.chat.global.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
 
 @Configuration
-class RedisConfig
+class RedisConfig{
+  @Bean
+  fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<*, *> {
+    val template = RedisTemplate<ByteArray, ByteArray>()
+    template.setConnectionFactory(connectionFactory)
+    return template
+  }
+}
 

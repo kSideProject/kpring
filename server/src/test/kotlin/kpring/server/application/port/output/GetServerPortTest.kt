4b6abef724a858a3f05ba142ef1d50fc9085415c
yudonggeun <ydong98@gmail.com>
2024-05-17 23:25:55 +0900

    test : 가입한 서버 목록 조회 output port 테스트

--- server/src/test/kotlin/kpring/server/application/port/output/GetServerPortTest.kt
+++ server/src/test/kotlin/kpring/server/application/port/output/GetServerPortTest.kt
@@ -2,9 +2,15 @@ package kpring.server.application.port.output
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import kpring.core.global.exception.CommonErrorCode
 import kpring.core.global.exception.ServiceException
+import kpring.server.adapter.output.mongo.entity.ServerEntity
+import kpring.server.adapter.output.mongo.entity.ServerUserEntity
+import kpring.server.adapter.output.mongo.repository.ServerRepository
+import kpring.server.domain.ServerUser
 import kpring.test.testcontainer.SpringTestContext
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
@@ -12,19 +18,72 @@ import org.springframework.test.context.ContextConfiguration
 @SpringBootTest
 @ContextConfiguration(initializers = [SpringTestContext.SpringDataMongo::class])
 class GetServerPortTest(
-  val getServerPort: GetServerPort,
+    val getServerPort: GetServerPort,
+    val serverRepository: ServerRepository,
 ) : DescribeSpec({
     it("존재하지 서버 id를 조회하는 경우 에러가 발생한다.") {
-      // given
-      val notExistServerId = "notExistServerId"
+        // given
+        val notExistServerId = "notExistServerId"
 
-      // when
-      val ex =
-        shouldThrow<ServiceException> {
-          getServerPort.get(notExistServerId)
+        // when
+        val ex =
+            shouldThrow<ServiceException> {
+                getServerPort.get(notExistServerId)
+            }
+
+        // then
+        ex.errorCode shouldBe CommonErrorCode.NOT_FOUND
+    }
+
+    it("저장된 서버의 정보를 조회할 수 있다.") {
+        // given
+        val serverUserEntities =
+            mutableListOf(
+                ServerUserEntity("id", "name", "/path"),
+            )
+        val serverEntity =
+            serverRepository.save(
+                ServerEntity(name = "test", users = serverUserEntities),
+            )
+
+        // when
+        val server = getServerPort.get(serverEntity.id)
+
+        // then
+        server.name shouldBe "test"
+        server.users shouldHaveSize 1
+        server.users shouldContain ServerUser("id", "name", "/path")
+    }
+
+    it("존재하지 않은 서버를 조회하면 예외가 발생한다.") {
+        // when
+        val exception =
+            shouldThrow<ServiceException> {
+                getServerPort.get("not-exist")
+            }
+
+        // then
+        exception.errorCode shouldBe CommonErrorCode.NOT_FOUND
+    }
+
+    it("유저가 속한 서버 목록을 조회할 수 있다.") {
+        // given
+        val userId = "test-user"
+        val serverUserEntities =
+            mutableListOf(ServerUserEntity(userId, "name", "/path"))
+
+        repeat(2) {
+            serverRepository.save(ServerEntity(name = "test${it}", users = serverUserEntities))
         }
 
-      // then
-      ex.errorCode shouldBe CommonErrorCode.NOT_FOUND
+        // when
+        val servers = getServerPort.getServerWith(userId)
+
+        // then
+        servers shouldHaveSize 2
+        servers.forEach {
+            it.users shouldHaveSize 1
+            it.users shouldContain ServerUser(userId, "name", "/path")
+        }
     }
-  })
+})

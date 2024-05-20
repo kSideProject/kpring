5a5cd3bfa3d233fe8e955153b221e4a978226240
allone9425 <allone9425@gmail.com>
2024-05-20 13:35:06 +0900

    [refac] 기존 Mui 테마 코드를 themeConfig.ts로 이동

---
+++ front/src/theme/themeConfig.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#FDE2F3",
+      main: "#917FB3",
+      dark: "#E5BEEC",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#2A2F4F",
+      main: "#2A2F4F",
+      dark: "#E5BEEC",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export default theme;

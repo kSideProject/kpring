ab0273804f4d3f89c2bbe5e15f34ab0a5e89e9cd
jihyun-j <jihyun9142@gmail.com>
2024-05-21 17:47:52 +0900

    [feat] Home 페이지에 carousel 추가

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,13 +1,33 @@
 import ServerCard from "../components/Home/ServerCard";
+import Carousel from "react-material-ui-carousel";
+import { serverData } from "../utils/fakeData";
+import { Button } from "@mui/material";
 
 const Home = () => {
   return (
-    <div className="my-24 mx-28">
-      <div className="">인기있는 서버들</div>
+    <div className="my-24 mx-36 text-cente">
+      <div className="flex flex-col gap-3 mb-12">
+        <div className="text-2xl">인기있는 서버들</div>
+        <Carousel autoPlay={false} height={400}>
+          {serverData.map((server) => {
+            return (
+              <div className="h-[400px]">
+                <img src={server.image} alt={server.serverName} />
+                <p className="absolute bottom-10 left-2 text-3xl">
+                  {server.serverName}
+                </p>
+                <Button>서버입장</Button>
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
 
       {/* API에 사용자가 서버에 접속한 마지막 시간을 기록할 수 있나요? */}
-      <h2 className="text-2xl">최근에 방문한 서버</h2>
-      <ServerCard />
+      <div className="flex flex-col gap-3">
+        <h2 className="text-2xl">최근에 방문한 서버</h2>
+        <ServerCard />
+      </div>
     </div>
   );
 };

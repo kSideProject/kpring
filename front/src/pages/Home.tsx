dd4affa00fe80c29fdd268b90a7b07feca0b6416
jihyun-j <jihyun9142@gmail.com>
2024-05-21 17:08:04 +0900

    [chore] mui-carosel 라이브러리 설치

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,5 +1,3 @@
-import { Box } from "@mui/material";
-import React from "react";
 import ServerCard from "../components/Home/ServerCard";
 
 const Home = () => {
@@ -7,6 +5,7 @@ const Home = () => {
     <div className="my-24 mx-28">
       <div className="">인기있는 서버들</div>
 
+      {/* API에 사용자가 서버에 접속한 마지막 시간을 기록할 수 있나요? */}
       <h2 className="text-2xl">최근에 방문한 서버</h2>
       <ServerCard />
     </div>

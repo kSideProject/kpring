e5028c0981bdf40b4af24a8deec99152100ef79b
minisun <minisundev@gmail.com>
2024-05-21 19:57:09 +0900

    feat: 낙관적 락 충돌 ErrorCode 정의

--- chat/src/main/kotlin/kpring/chat/global/exception/ErrorCode.kt
+++ chat/src/main/kotlin/kpring/chat/global/exception/ErrorCode.kt
@@ -3,8 +3,6 @@ package kpring.chat.global.exception
 import org.springframework.http.HttpStatus
 
 enum class ErrorCode(val httpStatus: Int, val message: String) {
-  // 400
-  INVALID_TOKEN(HttpStatus.BAD_REQUEST.value(), "토큰이 유효하지 않습니다"),
   USER_ALREADY_EXIST_IN_CHATROOM(HttpStatus.BAD_REQUEST.value(), "이미 참여한 사용자 입니다"),
 
   // 401
@@ -12,4 +10,7 @@ enum class ErrorCode(val httpStatus: Int, val message: String) {
 
   // 404
   CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 id로 chatroom을 찾을 수 없습니다"),
+
+  //409
+  LOCKING_FAILURE(HttpStatus.CONFLICT.value(), "충돌이 발생했습니다."),
 }

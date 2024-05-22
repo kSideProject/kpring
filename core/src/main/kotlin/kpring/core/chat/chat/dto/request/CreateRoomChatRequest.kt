b81ee588fbaaeea68ea9d2032452037783a6b229
minisun <minisundev@gmail.com>
2024-05-22 16:41:23 +0900

    refac: CreateChatRequest를 CreateRoomChatRequest로 rename

--- core/src/main/kotlin/kpring/core/chat/chat/dto/request/CreateRoomChatRequest.kt
+++ core/src/main/kotlin/kpring/core/chat/chat/dto/request/CreateRoomChatRequest.kt
@@ -3,7 +3,7 @@ package kpring.core.chat.chat.dto.request
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
-data class CreateChatRequest(
+data class CreateRoomChatRequest(
   @field:NotNull
   val room: String,
   @field:NotBlank

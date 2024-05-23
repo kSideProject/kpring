c54c85ba9b37e92f08894b61432e6fff16c799cd
minahYu <mina010311@naver.com>
2024-05-23 17:15:23 +0900

    feat: 프로필 수정 요청 dto에 필드 추가

--- user/src/main/java/kpring/user/dto/request/UpdateUserProfileRequest.java
+++ user/src/main/java/kpring/user/dto/request/UpdateUserProfileRequest.java
@@ -1,9 +1,14 @@
 package kpring.user.dto.request;
 
 import lombok.Builder;
+import org.springframework.web.multipart.MultipartFile;
 
 @Builder
 public record UpdateUserProfileRequest(
-        String email
+    String email,
+    String username,
+    String password,
+    String newPassword
 ) {
+
 }

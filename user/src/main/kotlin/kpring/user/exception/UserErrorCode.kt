db5af5a7cb2cfcb427021087490523e8546c89cb
minahYu <mina010311@naver.com>
2024-05-23 18:04:37 +0900

    feat: 이미지 파일 확장자가 지원되지 않는 경우에 대한 예외 처리 추가

--- user/src/main/kotlin/kpring/user/exception/UserErrorCode.kt
+++ user/src/main/kotlin/kpring/user/exception/UserErrorCode.kt
@@ -10,11 +10,12 @@ enum class UserErrorCode(
 ) : ErrorCode {
   BAD_REQUEST(HttpStatus.BAD_REQUEST, "4000", "Invalid email"),
   ALREADY_EXISTS_EMAIL(HttpStatus.BAD_REQUEST, "4001", "Email already exists"),
-  NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "4002", "Password does not match"),
   NOT_ALLOWED(HttpStatus.FORBIDDEN, "4003", "Not allowed"), // 권한이 없는 경우
 
   INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED, "4010", "Incorrect password"),
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "4011", "User not found"),
+
+  EXTENSION_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "4020", "Extension is not supported"),
   ;
 
   override fun message(): String = this.message

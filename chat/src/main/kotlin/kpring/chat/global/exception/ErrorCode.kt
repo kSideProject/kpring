6fa434949b9be6194bd573ca3626c4460e2197c6
minisun <minisundev@gmail.com>
2024-05-27 17:39:36 +0900

    feat: ErrorCode 정의

--- chat/src/main/kotlin/kpring/chat/global/exception/ErrorCode.kt
+++ chat/src/main/kotlin/kpring/chat/global/exception/ErrorCode.kt
@@ -13,4 +13,7 @@ enum class ErrorCode(val httpStatus: Int, val message: String) {
   INVALID_TOKEN_BODY(HttpStatus.NOT_FOUND.value(), "토큰의 body가 유효하지 않습니다"),
   USERID_NOT_EXIST(HttpStatus.NOT_FOUND.value(), "토큰의 userId가 존재하지 않습니다"),
   CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 id로 chatroom을 찾을 수 없습니다"),
+
+  // 504
+  MISSING_RESPONSE(HttpStatus.GATEWAY_TIMEOUT.value(), "요청의 응답이 올바르게 돌아오지 않았습니다."),
 }

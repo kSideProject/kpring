7a7985d5957cdb60fd8798548f4de236c5830942
minisun <minisundev@gmail.com>
2024-05-27 17:34:43 +0900

    feat: Server의 전체 Chat 조회 기능 Controller Layer 구현

--- chat/src/main/kotlin/kpring/chat/chat/api/v1/ChatController.kt
+++ chat/src/main/kotlin/kpring/chat/chat/api/v1/ChatController.kt
@@ -1,9 +1,13 @@
 package kpring.chat.chat.api.v1
 
 import kpring.chat.chat.service.ChatService
+import kpring.chat.global.exception.ErrorCode
+import kpring.chat.global.exception.GlobalException
 import kpring.core.auth.client.AuthClient
 import kpring.core.chat.chat.dto.request.CreateRoomChatRequest
 import kpring.core.chat.chat.dto.request.CreateServerChatRequest
+import kpring.core.global.dto.response.ApiResponse
+import kpring.core.server.client.ServerClient
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.*
 class ChatController(
   private val chatService: ChatService,
   val authClient: AuthClient,
+  val serverClient: ServerClient,
 ) {
   @PostMapping("/chat/chatroom")
   fun createRoomChat(
@@ -43,5 +48,27 @@ class ChatController(
     val userId = authClient.getTokenInfo(token).data!!.userId
     val result = chatService.getChatsByChatRoom(chatRoomId, userId, page)
     return ResponseEntity.ok().body(result)
+  }
+
+  @GetMapping("/chat/server/{serverId}")
+  fun getServerChats(
+    @PathVariable("serverId") serverId: String,
+    @RequestParam("page") page: Int,
+    @RequestHeader("Authorization") token: String,
+  ): ResponseEntity<*> {
+    val userId = authClient.getTokenInfo(token).data!!.userId
+    verifyIfUserHasJoinedServer(userId, serverId)
+    val result = chatService.getChatsByServer(serverId, userId, page)
+    return ResponseEntity.ok().body(ApiResponse(data = result))
+  }
+
+  fun verifyIfUserHasJoinedServer(
+    userId: String,
+    serverId: String,
+  ) {
+    val verified: Boolean = serverClient.verifyIfJoined(userId, serverId)
+    if (!verified) {
+      throw GlobalException(ErrorCode.MISSING_RESPONSE)
+    }
   }
 }

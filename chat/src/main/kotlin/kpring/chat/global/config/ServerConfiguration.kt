1d61c9ae65cea1cd45e58cc3f937d08c6944044d
minisun <minisundev@gmail.com>
2024-05-27 17:46:56 +0900

    feat: ServerConfiguration 정의

---
+++ chat/src/main/kotlin/kpring/chat/global/config/ServerConfiguration.kt
@@ -0,0 +1,9 @@
+package kpring.chat.global.config
+
+import kpring.core.server.config.ServerClientConfig
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+
+@Import(ServerClientConfig::class)
+@Configuration
+class ServerConfiguration

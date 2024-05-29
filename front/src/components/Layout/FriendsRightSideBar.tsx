a1f5985a2412057da5ba1d8cad469254095ed151
Ppangsoon she is gorgeous <81604180+yewon830@users.noreply.github.com>
2024-05-29 21:09:18 +0900

    feat : 사이드바 UI 수정 및 채팅방 컴포넌트 생성

--- front/src/components/Layout/FriendsRightSideBar.tsx
+++ front/src/components/Layout/FriendsRightSideBar.tsx
@@ -1,59 +1,69 @@
 import {
-  Avatar,
-  Badge,
+  Box,
   Divider,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
   styled,
+  TextField,
 } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { SideBarProps } from "../../types/layout";
-import React from "react";
-import MemberProfile from "../Profile/MemberProfile";
+import React, { useState } from "react";
+import DropDown from "../Layout/DropDown";
+import MemberList from "./MemberList";
+import { messageMemberList } from "../../utils/fakeData";
 
 const FriendsRightSideBar: React.FC<SideBarProps> = ({ close }) => {
   const DrawerHeader = styled("div")(({ theme }) => ({
     display: "flex",
+    height: "8rem",
     alignItems: "center",
     padding: theme.spacing(0, 1),
     ...theme.mixins.toolbar,
     justifyContent: "flex-start",
   }));
 
-  const [openProfile, setOpenProfile] = React.useState(false);
-  const handleOpen = () => setOpenProfile(true);
-  const handleClose = () => setOpenProfile(false);
+  const [openDropDown, setOpenDropDown] = useState(false);
+  const toggleDropDown = () => setOpenDropDown(!openDropDown);
+  const dropDownItems = ['디엠', '그룹', '전체 메세지']
+
   return (
     <>
-      <DrawerHeader>
-        <ArrowForwardIosIcon onClick={close} />
-        <div>친구목록</div>
+      <DrawerHeader className="flex-col">
+        <Box className="w-full flex py-3 px-1">
+          <ArrowForwardIosIcon onClick={close} className="cursor-pointer"/>
+          <Box className="w-full flex justify-between">
+            <div className="pl-1">친구 목록</div>
+            <Box className="relative">
+              { openDropDown?
+                <KeyboardArrowUpIcon 
+                  sx={{color: "purple"}}
+                  className="cursor-pointer" 
+                  onClick={toggleDropDown}
+                />:
+                <KeyboardArrowDownIcon className="cursor-pointer" onClick={toggleDropDown}/>
+              }
+              { openDropDown && 
+                <DropDown dropDownItems={dropDownItems}/>
+              }
+            </Box>
+          </Box>
+        </Box>
+        <TextField 
+          placeholder="친구 검색" 
+          variant="filled"
+          InputProps={{
+            sx: {
+              '& .MuiFilledInput-input': {
+                padding: '15px 12px 8px 12px', // 내부 input 요소의 패딩
+              },
+            }
+          }} 
+          sx={{paddingBottom: "1rem"}}
+          />
       </DrawerHeader>
       <Divider />
-      <List>
-        <ListItem>
-          <ListItemAvatar onClick={handleOpen}>
-            <Badge
-              color="success"
-              variant="dot"
-              overlap="circular"
-              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-            >
-              <Avatar
-                alt="user nickname"
-                src="https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              />
-            </Badge>
-          </ListItemAvatar>
-          <ListItemText primary="민선님" />
-        </ListItem>
-      </List>
-      <MemberProfile
-        openModal={openProfile}
-        closeModal={handleClose}
-      ></MemberProfile>
+      <MemberList memberList={messageMemberList}/>
     </>
   );
 };

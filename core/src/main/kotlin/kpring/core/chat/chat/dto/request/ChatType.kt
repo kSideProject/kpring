019b1669128a70970646a6a172661d2075502e2c
minisun <minisundev@gmail.com>
2024-05-30 16:44:41 +0900

    feat: ChatType enum 생성

---
+++ core/src/main/kotlin/kpring/core/chat/chat/dto/request/ChatType.kt
@@ -0,0 +1,10 @@
+package kpring.core.chat.chat.dto.request
+
+enum class ChatType(val type: String) {
+  Room("Room"),
+  Server("Server");
+
+  override fun toString(): String{
+    return type;
+  }
+}

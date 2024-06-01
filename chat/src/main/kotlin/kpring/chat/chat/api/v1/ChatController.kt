7622b6d5c1a4868d3b65ce808e72207d76160f11
minisun <minisundev@gmail.com>
2024-06-02 00:03:15 +0900

    feat: getChats method Controller Layer에 구현

--- chat/src/main/kotlin/kpring/chat/chat/api/v1/ChatController.kt
+++ chat/src/main/kotlin/kpring/chat/chat/api/v1/ChatController.kt
@@ -1,13 +1,13 @@
 package kpring.chat.chat.api.v1
 
 import kpring.chat.chat.service.ChatService
-import kpring.chat.global.exception.ErrorCode
-import kpring.chat.global.exception.GlobalException
 import kpring.core.auth.client.AuthClient
 import kpring.core.chat.chat.dto.request.CreateRoomChatRequest
 import kpring.core.chat.chat.dto.request.CreateServerChatRequest
+import kpring.core.chat.chat.dto.response.ChatResponse
 import kpring.core.global.dto.response.ApiResponse
 import kpring.core.server.client.ServerClient
+import kpring.core.server.dto.request.GetServerCondition
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -39,36 +39,23 @@ class ChatController(
     return ResponseEntity.ok().body(result)
   }
 
-  @GetMapping("/chat/{chatRoomId}/{page}")
-  fun getChatsByChatRoom(
-    @PathVariable("chatRoomId") chatRoomId: String,
-    @PathVariable("page") page: Int,
-    @RequestHeader("Authorization") token: String,
-  ): ResponseEntity<*> {
-    val userId = authClient.getTokenInfo(token).data!!.userId
-    val result = chatService.getChatsByChatRoom(chatRoomId, userId, page)
-    return ResponseEntity.ok().body(result)
-  }
-
-  @GetMapping("/chat/server/{serverId}")
-  fun getServerChats(
-    @PathVariable("serverId") serverId: String,
+  @GetMapping("/chat")
+  fun getChats(
+    @RequestParam("type") type: String,
+    @RequestParam("id") id: String,
     @RequestParam("page") page: Int,
     @RequestHeader("Authorization") token: String,
   ): ResponseEntity<*> {
     val userId = authClient.getTokenInfo(token).data!!.userId
-    verifyIfUserHasJoinedServer(userId, serverId)
-    val result = chatService.getChatsByServer(serverId, userId, page)
-    return ResponseEntity.ok().body(ApiResponse(data = result))
-  }
+    var result: ApiResponse<List<ChatResponse>>? = null
 
-  fun verifyIfUserHasJoinedServer(
-    userId: String,
-    serverId: String,
-  ) {
-    val verified: Boolean = serverClient.verifyIfJoined(userId, serverId)
-    if (!verified) {
-      throw GlobalException(ErrorCode.MISSING_RESPONSE)
+    if (type.equals("Room")) {
+      result = chatService.getRoomChats(id, userId, page)
+    } else if (type.equals("Server")) {
+      val serverList = serverClient.getServerList(token, GetServerCondition()).body!!.data!!
+      result = chatService.getServerChats(id, userId, page, serverList)
     }
+
+    return ResponseEntity.ok().body(result)
   }
 }

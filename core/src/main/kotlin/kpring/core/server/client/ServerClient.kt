5cb98e6b58af5bd758c9a19d806fbfc85506e0a8
minisun <minisundev@gmail.com>
2024-06-02 00:10:03 +0900

    refac: ServerClient에 getServerList 추가

--- core/src/main/kotlin/kpring/core/server/client/ServerClient.kt
+++ core/src/main/kotlin/kpring/core/server/client/ServerClient.kt
@@ -1,11 +1,17 @@
 package kpring.core.server.client
 
+import kpring.core.global.dto.response.ApiResponse
+import kpring.core.server.dto.ServerSimpleInfo
+import kpring.core.server.dto.request.GetServerCondition
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.service.annotation.GetExchange
 
 interface ServerClient {
-  @GetExchange("/api/v1/server/{serverId}/user/{userId}")
-  fun verifyIfJoined(
-    userId: String,
-    serverId: String,
-  ): Boolean
+  @GetExchange("/api/v1/server")
+  fun getServerList(
+    @RequestHeader("Authorization") token: String,
+    @ModelAttribute condition: GetServerCondition,
+  ): ResponseEntity<ApiResponse<List<ServerSimpleInfo>>>
 }

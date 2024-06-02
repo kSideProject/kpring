9ebca7537e911e42bc82ccaf85130148420304d8
minisun <minisundev@gmail.com>
2024-06-02 17:50:19 +0900

    feat: Controller Layer에 getChats method 구현

--- chat/src/main/kotlin/kpring/chat/chat/api/v1/ChatController.kt
+++ chat/src/main/kotlin/kpring/chat/chat/api/v1/ChatController.kt
@@ -3,6 +3,10 @@ package kpring.chat.chat.api.v1
 import kpring.chat.chat.service.ChatService
 import kpring.core.auth.client.AuthClient
 import kpring.core.chat.chat.dto.request.CreateChatRequest
+import kpring.core.chat.chat.dto.response.ChatResponse
+import kpring.core.global.dto.response.ApiResponse
+import kpring.core.server.client.ServerClient
+import kpring.core.server.dto.request.GetServerCondition
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*
 class ChatController(
   private val chatService: ChatService,
   val authClient: AuthClient,
+  val serverClient: ServerClient,
 ) {
   @PostMapping("/chat")
   fun createChat(
@@ -23,14 +28,23 @@ class ChatController(
     return ResponseEntity.ok().body(result)
   }
 
-  @GetMapping("/chat/{chatRoomId}/{page}")
-  fun getChatsByChatRoom(
-    @PathVariable("chatRoomId") chatRoomId: String,
-    @PathVariable("page") page: Int,
+  @GetMapping("/chat")
+  fun getChats(
+    @RequestParam("type") type: String,
+    @RequestParam("id") id: String,
+    @RequestParam("page") page: Int,
     @RequestHeader("Authorization") token: String,
   ): ResponseEntity<*> {
     val userId = authClient.getTokenInfo(token).data!!.userId
-    val result = chatService.getChatsByChatRoom(chatRoomId, userId, page)
-    return ResponseEntity.ok().body(result)
+    var result: List<ChatResponse>? = null
+
+    if (type.equals("Room")) {
+      result = chatService.getRoomChats(id, userId, page)
+    } else if (type.equals("Server")) {
+      val serverList = serverClient.getServerList(token, GetServerCondition()).body!!.data!!
+      result = chatService.getServerChats(id, userId, page, serverList)
+    }
+
+    return ResponseEntity.ok().body(ApiResponse(data = result, status = 200))
   }
 }

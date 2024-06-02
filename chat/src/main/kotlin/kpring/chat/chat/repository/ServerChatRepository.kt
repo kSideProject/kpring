848052ac7555c2add4ab86e47730ccd170842c62
minisun <minisundev@gmail.com>
2024-06-02 17:51:58 +0900

    feat: ServerChatRepository 생성

---
+++ chat/src/main/kotlin/kpring/chat/chat/repository/ServerChatRepository.kt
@@ -0,0 +1,15 @@
+package kpring.chat.chat.repository
+
+import kpring.chat.chat.model.ServerChat
+import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.querydsl.QuerydslPredicateExecutor
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ServerChatRepository : MongoRepository<ServerChat, String>, QuerydslPredicateExecutor<ServerChat> {
+  fun findAllByServerId(
+    serverId: String,
+    pageable: Pageable,
+  ): List<ServerChat>
+}

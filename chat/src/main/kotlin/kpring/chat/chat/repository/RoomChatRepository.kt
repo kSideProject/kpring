cb614a1fb9aa5e352efe1220248a99de99ccffe9
minisun <minisundev@gmail.com>
2024-06-02 17:54:03 +0900

    refac: Chat을 RoomChat으로 변경

--- chat/src/main/kotlin/kpring/chat/chat/repository/RoomChatRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chat/repository/RoomChatRepository.kt
@@ -1,15 +1,15 @@
 package kpring.chat.chat.repository
 
-import kpring.chat.chat.model.Chat
+import kpring.chat.chat.model.RoomChat
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ChatRepository : MongoRepository<Chat, String>, QuerydslPredicateExecutor<Chat> {
+interface RoomChatRepository : MongoRepository<RoomChat, String>, QuerydslPredicateExecutor<RoomChat> {
   fun findAllByRoomId(
     roomId: String,
     pageable: Pageable,
-  ): List<Chat>
+  ): List<RoomChat>
 }

5b70b4b92d8d91a5e32b1d0123aa87651e8bdb02
minisun <minisundev@gmail.com>
2024-06-04 02:08:13 +0900

    feat: UserChatRoomInvitationRepository에 setInvitation, getInvitation 메서드 구현

--- chat/src/main/kotlin/kpring/chat/chatroom/repository/UserChatRoomInvitationRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chatroom/repository/UserChatRoomInvitationRepository.kt
@@ -16,4 +16,48 @@ class UserChatRoomInvitationRepository(
   private val propertyConfig: PropertyConfig,
   private val invitationChatRoomRepository: InvitationChatRoomRepository,
 ) {
+  fun getInvitation(
+    userId: String,
+    chatRoomId: String,
+  ): InvitationResponse {
+    val key = generateKey(userId, chatRoomId)
+    var value = redisTemplate.opsForValue().get(key)
+    if (value == null) {
+      value = setInvitation(key, chatRoomId)
+    }
+
+    val expiration = getExpiration(key)
+
+    return InvitationResponse(value, LocalDateTime.now().plusSeconds(expiration).toString())
+  }
+
+  fun setInvitation(
+    key: String,
+    chatRoomId: String,
+  ): String {
+    val invitationLink = generateLink()
+    val ops: ValueOperations<String, String> = redisTemplate.opsForValue()
+    ops.set(key, invitationLink, propertyConfig.getExpiration())
+    invitationChatRoomRepository.setInvitationLink(invitationLink, chatRoomId)
+    return invitationLink
+  }
+
+  private fun generateKey(
+    userId: String,
+    chatRoomId: String,
+  ): String {
+    return "$userId:$chatRoomId"
+  }
+
+  private fun generateLink(): String {
+    return UUID.randomUUID().toString()
+  }
+
+  private fun getExpiration(key: String): Long {
+    val expiration = redisTemplate.getExpire(key)
+    if (expiration == null || expiration == -1L) {
+      throw GlobalException(ErrorCode.GET_EXPIRATION_FAILURE)
+    }
+    return expiration
+  }
 }

0a934bad4395879771b1368b83afd27573793e85
minisun <minisundev@gmail.com>
2024-06-04 02:10:00 +0900

    feat: InvitationChatRoomRepository에 setInvitationLink 메서드 구현

--- chat/src/main/kotlin/kpring/chat/chatroom/repository/InvitationChatRoomRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chatroom/repository/InvitationChatRoomRepository.kt
@@ -8,4 +8,19 @@ import org.springframework.data.redis.core.ValueOperations
 import org.springframework.stereotype.Component
 
 @Component
-class InvitationChatRoomRepository
+class InvitationChatRoomRepository(
+  private val redisTemplate: RedisTemplate<String, String>,
+  private val propertyConfig: PropertyConfig,
+) {
+  fun setInvitationLink(
+    invitationLink: String,
+    chatRoomId: String,
+  ) {
+    val ops: ValueOperations<String, String> = redisTemplate.opsForValue()
+    val result = ops.setIfAbsent(invitationLink, chatRoomId, propertyConfig.getExpiration())
+
+    if (result == null || !result) {
+      throw GlobalException(ErrorCode.INVITATION_LINK_SAVE_FAILURE)
+    }
+  }
+}

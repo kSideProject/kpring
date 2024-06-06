0449e6f4666e85443e60769b033ed90fbeec845b
minisun <minisundev@gmail.com>
2024-06-06 22:23:28 +0900

    refac: InvitationRepository의 재사용 불가능한 로직 InvitationService로 분리

--- chat/src/main/kotlin/kpring/chat/chatroom/repository/InvitationRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chatroom/repository/InvitationRepository.kt
@@ -1,67 +1,28 @@
 package kpring.chat.chatroom.repository
 
-import kpring.chat.global.config.ChatRoomProperty
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.core.ValueOperations
 import org.springframework.stereotype.Component
-import java.nio.charset.StandardCharsets
-import java.security.MessageDigest
-import java.util.*
+import java.time.Duration
 
 @Component
 class InvitationRepository(
   private val redisTemplate: RedisTemplate<String, String>,
-  private val chatRoomProperty: ChatRoomProperty,
 ) {
-  fun getInvitationCode(
-    userId: String,
-    chatRoomId: String,
-  ): String {
-    val key = generateKey(userId, chatRoomId)
-    var value = redisTemplate.opsForValue().get(key)
-    if (value == null) {
-      value = setInvitation(key, chatRoomId)
-    }
-    return generateCode(key, value)
+  fun getValue(key: String): String? {
+    return redisTemplate.opsForValue().get(key)
   }
 
-  fun setInvitation(
-    key: String,
-    chatRoomId: String,
-  ): String {
-    val value = generateValue()
-    val ops: ValueOperations<String, String> = redisTemplate.opsForValue()
-    ops.set(key, value, chatRoomProperty.getExpiration())
-    return value
-  }
-
-  fun getExpiration(
-    userId: String,
-    chatRoomId: String,
-  ): Long {
-    val key = generateKey(userId, chatRoomId)
-    val expiration = redisTemplate.getExpire(key)
-    return expiration
-  }
-
-  fun generateCode(
+  fun setValueAndExpiration(
     key: String,
     value: String,
-  ): String {
-    val combine = "$key,$value"
-    val digest = MessageDigest.getInstance("SHA-256")
-    val hash = digest.digest(combine.toByteArray(StandardCharsets.UTF_8))
-    return Base64.getEncoder().encodeToString(hash)
+    expiration: Duration,
+  ) {
+    val ops: ValueOperations<String, String> = redisTemplate.opsForValue()
+    ops.set(key, value, expiration)
   }
 
-  private fun generateKey(
-    userId: String,
-    chatRoomId: String,
-  ): String {
-    return "$userId:$chatRoomId"
-  }
-
-  private fun generateValue(): String {
-    return UUID.randomUUID().toString()
+  fun getExpiration(key: String): Long {
+    return redisTemplate.getExpire(key)
   }
 }

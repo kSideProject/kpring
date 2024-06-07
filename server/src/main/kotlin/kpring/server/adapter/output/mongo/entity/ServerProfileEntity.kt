cd456390e86e7ec228feba580dad65d8ba0e4dc5
yudonggeun <ydong98@gmail.com>
2024-06-07 21:09:07 +0900

    bug : server 생성시 serverProfileEntity id가 "test"로 고정되는 버그 수정

--- server/src/main/kotlin/kpring/server/adapter/output/mongo/entity/ServerProfileEntity.kt
+++ server/src/main/kotlin/kpring/server/adapter/output/mongo/entity/ServerProfileEntity.kt
@@ -6,11 +6,13 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("server_profile")
 class ServerProfileEntity(
-  @Id
   val userId: String,
   val name: String,
   val imagePath: String,
   val serverId: String,
   val role: ServerRole,
   val bookmarked: Boolean,
-)
+) {
+  @Id
+  private var id: String? = null
+}

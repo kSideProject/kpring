31a5584f4e752dd99ce65c441bdaa099bccee768
minisun <minisundev@gmail.com>
2024-06-08 23:50:31 +0900

    feat: ChatRoomServiceTest에 inviteToChatRoomByUserId 테스트코드 작성

--- chat/src/test/kotlin/kpring/chat/chatroom/ChatRoomServiceTest.kt
+++ chat/src/test/kotlin/kpring/chat/chatroom/ChatRoomServiceTest.kt
@@ -1,22 +1,30 @@
 package kpring.chat.chatroom
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.collections.shouldNotContain
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import kpring.chat.chatroom.dto.Lock
 import kpring.chat.chatroom.model.ChatRoom
 import kpring.chat.chatroom.repository.ChatRoomRepository
 import kpring.chat.chatroom.service.ChatRoomService
+import kpring.chat.chatroom.service.DistributedLockService
 import kpring.chat.global.ChatRoomTest
 import kpring.chat.global.CommonTest
+import kpring.chat.global.exception.ErrorCode
+import kpring.chat.global.exception.GlobalException
 import kpring.core.chat.chatroom.dto.request.CreateChatRoomRequest
 import java.util.*
 
 class ChatRoomServiceTest : FunSpec({
 
   val chatRoomRepository = mockk<ChatRoomRepository>()
-  val chatRoomService = ChatRoomService(chatRoomRepository)
+  val lockService = mockk<DistributedLockService>()
+  val chatRoomService = ChatRoomService(chatRoomRepository, lockService)
 
   test("createChatRoom 는 새 ChatRoom을 저장해야 한다") {
     // Given
@@ -48,5 +56,67 @@ class ChatRoomServiceTest : FunSpec({
     // Then
     verify { chatRoomRepository.save(chatRoom) }
     chatRoom.members.shouldNotContain(CommonTest.TEST_USER_ID)
+  }
+
+  test("inviteToChatRoomByUserIdWithLock 는 동시성 잠금을 획득한 후 사용자를 초대해야 한다") {
+    // Given
+    val chatRoom =
+      ChatRoom().apply {
+        id = ChatRoomTest.TEST_ROOM_ID
+        addUsers(ChatRoomTest.TEST_MEMBERS)
+      }
+    val lock = Lock("chatRoom:${chatRoom.id}:lock", UUID.randomUUID().toString(), true)
+
+    every { lockService.getLock(any()) } returns lock
+    every { lockService.releaseLock(any(), any()) } returns Unit
+    every { chatRoomRepository.findById(chatRoom.id!!) } returns Optional.of(chatRoom)
+    every { chatRoomRepository.save(any()) } returns chatRoom
+    every { chatRoomRepository.existsByIdAndMembersContaining(any(), CommonTest.TEST_USER_ID) } returns true
+
+    // When
+    chatRoomService.inviteToChatRoomByUserIdWithLock(CommonTest.TEST_USER_ID, CommonTest.TEST_USER_ID, chatRoom.id!!)
+
+    // Then
+    verify { lockService.getLock(any()) }
+    verify { chatRoomRepository.save(chatRoom) }
+    verify { lockService.releaseLock(any(), any()) }
+    chatRoom.members.shouldContain(CommonTest.TEST_USER_ID)
+  }
+
+  test("inviteToChatRoomByUserId 는 사용자를 초대해야 한다") {
+    // Given
+    val chatRoom =
+      ChatRoom().apply {
+        id = ChatRoomTest.TEST_ROOM_ID
+        addUsers(ChatRoomTest.TEST_MEMBERS)
+      }
+
+    every { chatRoomRepository.findById(chatRoom.id!!) } returns Optional.of(chatRoom)
+    every { chatRoomRepository.save(any()) } returns chatRoom
+    every { chatRoomRepository.existsByIdAndMembersContaining(any(), CommonTest.TEST_USER_ID) } returns true
+
+    // When
+    chatRoomService.inviteToChatRoomByUserId(CommonTest.TEST_USER_ID, CommonTest.TEST_USER_ID, chatRoom.id!!)
+
+    // Then
+    verify { chatRoomRepository.save(chatRoom) }
+    chatRoom.members.shouldContain(CommonTest.TEST_USER_ID)
+  }
+
+  test("inviteToChatRoomByUserIdWithLock 는 잠금 획득 실패 시 예외를 발생해야 한다") {
+    // Given
+    val chatRoomId = ChatRoomTest.TEST_ROOM_ID
+    val userId = CommonTest.TEST_USER_ID
+
+    every { lockService.getLock(any()) } throws GlobalException(ErrorCode.CONCURRENCY_CONFLICTION)
+
+    // When
+    val exception =
+      shouldThrow<GlobalException> {
+        chatRoomService.inviteToChatRoomByUserIdWithLock(userId, userId, chatRoomId)
+      }
+
+    // Then
+    exception.getErrorCode() shouldBe ErrorCode.CONCURRENCY_CONFLICTION
   }
 })

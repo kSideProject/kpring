f3e85a36fd639e8297e7c57e43b5bb1d98ba14c0
minisun <minisundev@gmail.com>
2024-06-08 23:48:06 +0900

    feat: DistributedLockService에 getLock 메서드 구현

--- chat/src/main/kotlin/kpring/chat/chatroom/service/DistributedLockService.kt
+++ chat/src/main/kotlin/kpring/chat/chatroom/service/DistributedLockService.kt
@@ -1,7 +1,10 @@
 package kpring.chat.chatroom.service
 
+import kpring.chat.chatroom.dto.Lock
 import kpring.chat.chatroom.model.DistributedLock
 import kpring.chat.chatroom.repository.DistributedLockRepository
+import kpring.chat.global.exception.ErrorCode
+import kpring.chat.global.exception.GlobalException
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -40,5 +43,16 @@ class DistributedLockService(
     if (optionalLock.isPresent && optionalLock.get().owner == owner) {
       lockRepository.deleteById(lockId)
     }
+  }
+
+  fun getLock(chatRoomId: String): Lock {
+    val lockId = "chatRoom:$chatRoomId:lock"
+    val owner = UUID.randomUUID().toString()
+
+    val lockAcquired = acquireLock(lockId, owner, 10000) // 10초 동안 잠금 유지
+    if (!lockAcquired) {
+      throw GlobalException(ErrorCode.CONCURRENCY_CONFLICTION)
+    }
+    return Lock(lockId = lockId, owner = owner, acquired = lockAcquired)
   }
 }

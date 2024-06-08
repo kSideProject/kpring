270e0618255739f77b53fa733afce8cde220ddf2
BOOK-7HF2F58CR4\sour1 <wonderwall00@naver.com>
2024-06-08 10:25:16 +0900

    remove : 깃 문제로 없어져야 하는 파일이 존재해서 삭제

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      // 기존 색상에서 확장 위해서 extend에 colors 넣는 방식으로 변경
       colors: {
         dark:{
           DEFAULT : "#2A2F4F",

734f133470c16f540cf257ab24415a2a9045043c
minisun <minisundev@gmail.com>
2024-06-08 23:44:22 +0900

    feat: inviteToChatRoomByUserId Service Layer에 구현

--- chat/src/main/kotlin/kpring/chat/chatroom/service/ChatRoomService.kt
+++ chat/src/main/kotlin/kpring/chat/chatroom/service/ChatRoomService.kt
@@ -6,10 +6,12 @@ import kpring.chat.global.exception.ErrorCode
 import kpring.chat.global.exception.GlobalException
 import kpring.core.chat.chatroom.dto.request.CreateChatRoomRequest
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ChatRoomService(
   private val chatRoomRepository: ChatRoomRepository,
+  private val lockService: DistributedLockService,
 ) {
   fun createChatRoom(
     request: CreateChatRoomRequest,
@@ -27,6 +29,31 @@ class ChatRoomService(
     verifyChatRoomAccess(chatRoomId, userId)
     val chatRoom: ChatRoom = getChatRoom(chatRoomId)
     chatRoom.removeUser(userId)
+    chatRoomRepository.save(chatRoom)
+  }
+
+  @Transactional
+  fun inviteToChatRoomByUserIdWithLock(
+    userId: String,
+    inviterId: String,
+    chatRoomId: String,
+  ): Boolean {
+    val lock = lockService.getLock(chatRoomId)
+
+    inviteToChatRoomByUserId(userId, inviterId, chatRoomId)
+
+    lockService.releaseLock(lock.lockId, lock.owner)
+    return true
+  }
+
+  fun inviteToChatRoomByUserId(
+    userId: String,
+    inviterId: String,
+    chatRoomId: String,
+  ) {
+    verifyChatRoomAccess(chatRoomId, inviterId)
+    val chatRoom: ChatRoom = getChatRoom(chatRoomId)
+    chatRoom.addUser(userId)
     chatRoomRepository.save(chatRoom)
   }
 

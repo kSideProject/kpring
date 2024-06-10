1a8ef8d90be1e8833e66ccf7dbc490705834c3f1
minisun <minisundev@gmail.com>
2024-06-10 16:37:52 +0900

    feat: Chat에 verifyAccess 메서드 구현

--- chat/src/main/kotlin/kpring/chat/chat/model/Chat.kt
+++ chat/src/main/kotlin/kpring/chat/chat/model/Chat.kt
@@ -1,6 +1,8 @@
 package kpring.chat.chat.model
 
 import kpring.chat.NoArg
+import kpring.chat.global.exception.ErrorCode
+import kpring.chat.global.exception.GlobalException
 import kpring.chat.global.model.BaseTime
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -22,5 +24,11 @@ class Chat(
 
   fun updateContent(content: String) {
     this.content = content
+  }
+
+  fun verifyAccess(userId: String) {
+    if (userId != this.userId) {
+      throw GlobalException(ErrorCode.FORBIDDEN_CHAT)
+    }
   }
 }

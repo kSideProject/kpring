44957fa6fcef970140ca24e77a83e6ea7c7a658f
minisun <minisundev@gmail.com>
2024-06-10 14:17:13 +0900

    refac: ServerChatRepository가 ServerChat대신 Chat을 저장하도록 변경

--- chat/src/main/kotlin/kpring/chat/chat/repository/ServerChatRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chat/repository/ServerChatRepository.kt
@@ -1,15 +1,15 @@
 package kpring.chat.chat.repository
 
-import kpring.chat.chat.model.ServerChat
+import kpring.chat.chat.model.Chat
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ServerChatRepository : MongoRepository<ServerChat, String>, QuerydslPredicateExecutor<ServerChat> {
-  fun findAllByServerId(
+interface ServerChatRepository : MongoRepository<Chat, String>, QuerydslPredicateExecutor<Chat> {
+  fun findAllByContextId(
     serverId: String,
     pageable: Pageable,
-  ): List<ServerChat>
+  ): List<Chat>
 }

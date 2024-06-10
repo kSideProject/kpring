afd73a7e6505ab0ea0b40b66016b379f1a76a69f
minahYu <mina010311@naver.com>
2024-06-10 18:45:17 +0900

    feat: FriendServiceImpl에 친구신청 기능 구현

--- user/src/main/kotlin/kpring/user/service/FriendServiceImpl.kt
+++ user/src/main/kotlin/kpring/user/service/FriendServiceImpl.kt
@@ -1,14 +1,22 @@
 package kpring.user.service
 
+import kpring.core.global.exception.ServiceException
 import kpring.user.dto.response.DeleteFriendResponse
 import kpring.user.dto.response.GetFriendsResponse
 import kpring.user.dto.result.AddFriendResponse
+import kpring.user.entity.FriendRequestStatus
+import kpring.user.entity.User
+import kpring.user.exception.UserErrorCode
+import kpring.user.repository.FriendRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
-class FriendServiceImpl() : FriendService {
+class FriendServiceImpl(
+  private val userServiceImpl: UserServiceImpl,
+  private val friendRepository: FriendRepository,
+) : FriendService {
   override fun getFriends(userId: Long): GetFriendsResponse {
     TODO("Not yet implemented")
   }
@@ -17,7 +25,15 @@ class FriendServiceImpl() : FriendService {
     userId: Long,
     friendId: Long,
   ): AddFriendResponse {
-    TODO("Not yet implemented")
+    val user = userServiceImpl.getUser(userId)
+    val friend = userServiceImpl.getUser(friendId)
+
+    checkSelfFriend(user, friend)
+    checkFriendRelationExists(userId, friendId)
+    user.addFriendRelation(friend, FriendRequestStatus.REQUESTED)
+    friend.addFriendRelation(user, FriendRequestStatus.RECEIVED)
+
+    return AddFriendResponse(friend.id!!)
   }
 
   override fun deleteFriend(
@@ -25,5 +41,23 @@ class FriendServiceImpl() : FriendService {
     friendId: Long,
   ): DeleteFriendResponse {
     TODO("Not yet implemented")
+  }
+
+  private fun checkSelfFriend(
+    user: User,
+    friend: User,
+  ) {
+    if (user == friend) {
+      throw ServiceException(UserErrorCode.NOT_SELF_FOLLOW)
+    }
+  }
+
+  private fun checkFriendRelationExists(
+    userId: Long,
+    friendId: Long,
+  ) {
+    if (friendRepository.existsByUserIdAndFriendId(userId, friendId)) {
+      throw ServiceException(UserErrorCode.ALREADY_FRIEND)
+    }
   }
 }

fce76eeb291c4d56a18ace4a932e6a77382cf5ed
minahYu <mina010311@naver.com>
2024-06-11 14:01:18 +0900

    test: 친구신청 controller 테스트코드 작성

---
+++ user/src/test/kotlin/kpring/user/controller/FriendControllerTest.kt
@@ -0,0 +1,194 @@
+package kpring.user.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import kpring.core.global.dto.response.ApiResponse
+import kpring.core.global.exception.ServiceException
+import kpring.test.restdoc.dsl.restDoc
+import kpring.test.restdoc.json.JsonDataType.Strings
+import kpring.user.dto.response.FailMessageResponse
+import kpring.user.dto.result.AddFriendResponse
+import kpring.user.exception.UserErrorCode
+import kpring.user.global.AuthValidator
+import kpring.user.global.CommonTest
+import kpring.user.service.FriendServiceImpl
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.restdocs.ManualRestDocumentation
+import org.springframework.restdocs.operation.preprocess.Preprocessors
+import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient
+import org.springframework.web.context.WebApplicationContext
+
+@WebMvcTest(controllers = [FriendController::class])
+@ExtendWith(value = [MockKExtension::class])
+internal class FriendControllerTest(
+  private val objectMapper: ObjectMapper,
+  webContext: WebApplicationContext,
+  @MockkBean val friendService: FriendServiceImpl,
+  @MockkBean val authValidator: AuthValidator,
+) : DescribeSpec({
+
+    val restDocument = ManualRestDocumentation()
+    val webTestClient =
+      MockMvcWebTestClient.bindToApplicationContext(webContext)
+        .configureClient()
+        .filter(
+          WebTestClientRestDocumentation.documentationConfiguration(restDocument)
+            .operationPreprocessors()
+            .withRequestDefaults(Preprocessors.prettyPrint())
+            .withResponseDefaults(Preprocessors.prettyPrint()),
+        )
+        .build()
+
+    beforeSpec { restDocument.beforeTest(this.javaClass, "friend controller") }
+
+    afterSpec { restDocument.afterTest() }
+
+    describe("친구신청 API") {
+      it("친구신청 성공") {
+        // given
+        val data = AddFriendResponse(friendId = CommonTest.TEST_FRIEND_ID)
+        val response = ApiResponse(data = data)
+        every {
+          authValidator.checkIfAccessTokenAndGetUserId(any())
+        } returns CommonTest.TEST_USER_ID.toString()
+        every { authValidator.checkIfUserIsSelf(any(), any()) } returns Unit
+        every {
+          friendService.addFriend(CommonTest.TEST_USER_ID, CommonTest.TEST_FRIEND_ID)
+        } returns data
+
+        // when
+        val result =
+          webTestClient.post()
+            .uri(
+              "/api/v1/user/{userId}/friend/{friendId}",
+              CommonTest.TEST_USER_ID,
+              CommonTest.TEST_FRIEND_ID,
+            )
+            .header("Authorization", CommonTest.TEST_TOKEN)
+            .exchange()
+
+        // then
+        val docsRoot =
+          result
+            .expectStatus().isOk
+            .expectBody().json(objectMapper.writeValueAsString(response))
+
+        // docs
+        docsRoot
+          .restDoc(
+            identifier = "addFriend200",
+            description = "친구신청 API",
+          ) {
+            request {
+              path {
+                "userId" mean "사용자 아이디"
+                "friendId" mean "친구신청을 받은 사용자의 아이디"
+              }
+              header { "Authorization" mean "Bearer token" }
+            }
+            response {
+              body {
+                "data.friendId" type Strings mean "친구신청을 받은 사용자의 아이디"
+              }
+            }
+          }
+      }
+
+      it("친구신청 실패 : 권한이 없는 토큰") {
+        // given
+        val response =
+          FailMessageResponse.builder().message(UserErrorCode.NOT_ALLOWED.message()).build()
+        every { authValidator.checkIfAccessTokenAndGetUserId(any()) } throws
+          ServiceException(
+            UserErrorCode.NOT_ALLOWED,
+          )
+
+        // when
+        val result =
+          webTestClient.post()
+            .uri(
+              "/api/v1/user/{userId}/friend/{friendId}",
+              CommonTest.TEST_USER_ID,
+              CommonTest.TEST_FRIEND_ID,
+            )
+            .header("Authorization", CommonTest.TEST_TOKEN)
+            .exchange()
+
+        // then
+        val docsRoot =
+          result
+            .expectStatus().isForbidden
+            .expectBody().json(objectMapper.writeValueAsString(response))
+
+        // docs
+        docsRoot
+          .restDoc(
+            identifier = "addFriend403",
+            description = "친구신청 API",
+          ) {
+            request {
+              path {
+                "userId" mean "사용자 아이디"
+                "friendId" mean "친구신청을 받은 사용자의 아이디"
+              }
+              header { "Authorization" mean "Bearer token" }
+            }
+            response {
+              body {
+                body { "message" type Strings mean "에러 메시지" }
+              }
+            }
+          }
+      }
+
+      it("친구신청 실패 : 서버 내부 오류") {
+        // given
+        every {
+          authValidator.checkIfAccessTokenAndGetUserId(any())
+        } throws RuntimeException("서버 내부 오류")
+        val response = FailMessageResponse.serverError
+
+        // when
+        val result =
+          webTestClient.post()
+            .uri(
+              "/api/v1/user/{userId}/friend/{friendId}",
+              CommonTest.TEST_USER_ID,
+              CommonTest.TEST_FRIEND_ID,
+            )
+            .header("Authorization", CommonTest.TEST_TOKEN)
+            .exchange()
+
+        // then
+        val docsRoot =
+          result
+            .expectStatus().isEqualTo(500)
+            .expectBody().json(objectMapper.writeValueAsString(response))
+
+        // docs
+        docsRoot
+          .restDoc(
+            identifier = "addFriend500",
+            description = "친구신청 API",
+          ) {
+            request {
+              path {
+                "userId" mean "사용자 아이디"
+                "friendId" mean "친구신청을 받은 사용자의 아이디"
+              }
+              header { "Authorization" mean "Bearer token" }
+            }
+            response {
+              body {
+                "message" type Strings mean "에러 메시지"
+              }
+            }
+          }
+      }
+    }
+  })

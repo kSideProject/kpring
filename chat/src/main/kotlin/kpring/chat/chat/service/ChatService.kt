ee3166e12bb6d7bafa470bd55c836a52a51fdf5d
minisun <minisundev@gmail.com>
2024-06-12 23:55:50 +0900

    feat: deleteRoomChat, deleteServerChat Service Layer에 구현

--- chat/src/main/kotlin/kpring/chat/chat/service/ChatService.kt
+++ chat/src/main/kotlin/kpring/chat/chat/service/ChatService.kt
@@ -101,6 +101,26 @@ class ChatService(
     return true
   }
 
+  fun deleteRoomChat(
+    chatId: String,
+    userId: String,
+  ): Boolean {
+    val chat = roomChatRepository.findById(chatId).orElseThrow { GlobalException(ErrorCode.CHAT_NOT_FOUND) }
+    verifyIfAuthor(userId, chat)
+    roomChatRepository.delete(chat)
+    return true
+  }
+
+  fun deleteServerChat(
+    chatId: String,
+    userId: String,
+  ): Boolean {
+    val chat = serverChatRepository.findById(chatId).orElseThrow { GlobalException(ErrorCode.CHAT_NOT_FOUND) }
+    verifyIfAuthor(userId, chat)
+    serverChatRepository.delete(chat)
+    return true
+  }
+
   private fun verifyIfAuthor(
     userId: String,
     chat: Chat,

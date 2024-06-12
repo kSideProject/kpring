a88474e5dd0aa913577ff34d0309f57473906075
minahYu <mina010311@naver.com>
2024-06-12 17:32:01 +0900

    chore: 상태별 주석 추가

--- user/src/main/kotlin/kpring/user/entity/FriendRequestStatus.kt
+++ user/src/main/kotlin/kpring/user/entity/FriendRequestStatus.kt
@@ -1,7 +1,7 @@
 package kpring.user.entity
 
 enum class FriendRequestStatus {
-  REQUESTED,
-  RECEIVED,
-  ACCEPTED,
+  REQUESTED, // 친구신청을 보낸 상태
+  RECEIVED, // 친구신청을 받은 상태
+  ACCEPTED, // 친구신청을 수락한(친구가 된) 상태
 }

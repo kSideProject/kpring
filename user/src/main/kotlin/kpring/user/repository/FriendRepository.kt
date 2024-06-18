e5dc63360e0b4a0d241859cb3706813487d3b7d0
minahYu <mina010311@naver.com>
2024-06-18 16:18:10 +0900

    feat: FriendRepository 에 findByUserIdAndFriendIdAndRequestStatus 메서드 추가

--- user/src/main/kotlin/kpring/user/repository/FriendRepository.kt
+++ user/src/main/kotlin/kpring/user/repository/FriendRepository.kt
@@ -3,6 +3,7 @@ package kpring.user.repository
 import kpring.user.entity.Friend
 import kpring.user.entity.FriendRequestStatus
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface FriendRepository : JpaRepository<Friend, Long> {
   fun existsByUserIdAndFriendId(
@@ -14,4 +15,10 @@ interface FriendRepository : JpaRepository<Friend, Long> {
     userId: Long,
     requestStatus: FriendRequestStatus,
   ): List<Friend>
+
+  fun findByUserIdAndFriendIdAndRequestStatus(
+    userId: Long,
+    friendId: Long,
+    requestStatus: FriendRequestStatus,
+  ): Optional<Friend>
 }

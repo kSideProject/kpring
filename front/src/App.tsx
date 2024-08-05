55dd3e7ffb07dc5f1aeafe0158334f9bbf8f42ed
allone9425 <allone9425@gmail.com>
2024-08-05 10:26:37 +0900

    [feat] 메인페이지 접근 시 로그인이 유효하지 않으면 로그인으로 리디렉션됨

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AuthLayout from "./components/Auth/AuthLayout";
+import PrivateRoute from "./components/Auth/PrivateRoute";
 import Layout from "./components/Layout/Layout";
 import { ServerMap } from "./components/Phaser/ServerMap";
 import Home from "./pages/Home";
 import Join from "./pages/Join";
 import Login from "./pages/Login";
+
 function App() {
   return (
     <div className="App">
@@ -15,12 +17,27 @@ function App() {
           <Route path="/join" element={<Join />} />
         </Route>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="server/:serverId" element={<ServerMap />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="server/:serverId"
+            element={
+              <PrivateRoute>
+                <ServerMap />
+              </PrivateRoute>
+            }
+          />
           <Route path="*" element={<Navigate replace to="/" />} />
         </Route>
       </Routes>
     </div>
   );
 }
+
 export default App;

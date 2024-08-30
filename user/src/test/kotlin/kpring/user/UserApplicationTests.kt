aa56d8369696f436bd4c44b79bc750cab692d21b
minahYu <mina010311@naver.com>
2024-08-30 15:52:20 +0900

    chore: 테스트에 dev 프로파일 활성화를 위해 코드 수정

--- user/src/test/kotlin/kpring/user/UserApplicationTests.kt
+++ user/src/test/kotlin/kpring/user/UserApplicationTests.kt
@@ -3,9 +3,11 @@ package kpring.user
 import kpring.test.testcontainer.SpringTestContext
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
+@ActiveProfiles("dev")
 @ContextConfiguration(initializers = [SpringTestContext.SpringDataJpaMySql::class])
 class UserApplicationTests {
   @Test

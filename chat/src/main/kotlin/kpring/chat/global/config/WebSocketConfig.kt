b6538427d7a951a2f57d0dc8cd4e6d2dc73e94ce
minisun <minisundev@gmail.com>
2024-09-01 20:00:20 +0900

    feat: STOMP 및 SockJS를 이용한 WebSocket 설정 추가

---
+++ chat/src/main/kotlin/kpring/chat/global/config/WebSocketConfig.kt
@@ -0,0 +1,21 @@
+package kpring.chat.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+  override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+    registry.addEndpoint("/ws")
+      .withSockJS()
+  }
+
+  override fun configureMessageBroker(config: MessageBrokerRegistry) {
+    config.setApplicationDestinationPrefixes("/app")
+    config.enableSimpleBroker("/topic")
+  }
+}

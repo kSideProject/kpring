c1e0404532fe89da8f22a89d92e72b636c83e2d2
minisun <minisundev@gmail.com>
2024-09-01 20:01:02 +0900

    feat: WebSocket 전송 설정에서 메시지 크기 제한 및 첫 메시지 타이밍 설정 추가

--- chat/src/main/kotlin/kpring/chat/global/config/WebSocketConfig.kt
+++ chat/src/main/kotlin/kpring/chat/global/config/WebSocketConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,5 +18,10 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
   override fun configureMessageBroker(config: MessageBrokerRegistry) {
     config.setApplicationDestinationPrefixes("/app")
     config.enableSimpleBroker("/topic")
+  }
+
+  override fun configureWebSocketTransport(registry: WebSocketTransportRegistration) {
+    registry.setMessageSizeLimit(4 * 8192)
+    registry.setTimeToFirstMessage(30000)
   }
 }

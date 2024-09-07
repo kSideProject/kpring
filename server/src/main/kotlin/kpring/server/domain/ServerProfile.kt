6399b5277949f4e41ca9854650e8abc3c65507be
minahYu <mina010311@naver.com>
2024-09-07 18:28:58 +0900

    feat: serverProfile 엔티티에 updateServerHost 메서드 추가

--- server/src/main/kotlin/kpring/server/domain/ServerProfile.kt
+++ server/src/main/kotlin/kpring/server/domain/ServerProfile.kt
@@ -6,13 +6,12 @@ class ServerProfile(
   val name: String,
   val imagePath: String,
   val server: Server,
-  val role: ServerRole = ServerRole.MEMBER,
+  var role: ServerRole = ServerRole.MEMBER,
   val bookmarked: Boolean = false,
 ) {
   /**
    * @param authority 확인할 권한
-   * @return 권한이 있으면 true, 없으면 false
-   */
+   * @return 권한이 있으면 true, 없으면 false   */
   fun hasRole(authority: ServerAuthority): Boolean {
     return role.contains(authority)
   }
@@ -22,5 +21,13 @@ class ServerProfile(
    */
   fun dontHasRole(authority: ServerAuthority): Boolean {
     return !hasRole(authority)
+  }
+
+  fun updateServerHost(serverProfile: ServerProfile) {
+    if (serverProfile.role == ServerRole.OWNER) {
+      serverProfile.role = ServerRole.MEMBER
+    } else if (serverProfile.role == ServerRole.MEMBER) {
+      serverProfile.role = ServerRole.OWNER
+    }
   }
 }

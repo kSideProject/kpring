dde4e934c3729788e08cc941c2171ffaee32af67
minahYu <mina010311@naver.com>
2024-09-07 18:26:44 +0900

    feat: Controller에 서버 권한 상속 메서드 추가

--- server/src/main/kotlin/kpring/server/adapter/input/rest/RestApiServerController.kt
+++ server/src/main/kotlin/kpring/server/adapter/input/rest/RestApiServerController.kt
@@ -7,10 +7,8 @@ import kpring.core.server.dto.ServerSimpleInfo
 import kpring.core.server.dto.request.AddUserAtServerRequest
 import kpring.core.server.dto.request.CreateServerRequest
 import kpring.core.server.dto.request.GetServerCondition
-import kpring.server.application.port.input.AddUserAtServerUseCase
-import kpring.server.application.port.input.CreateServerUseCase
-import kpring.server.application.port.input.DeleteServerUseCase
-import kpring.server.application.port.input.GetServerInfoUseCase
+import kpring.core.server.dto.request.UpdateHostAtServerRequest
+import kpring.server.application.port.input.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -21,6 +19,7 @@ class RestApiServerController(
   val getServerUseCase: GetServerInfoUseCase,
   val addUserAtServerUseCase: AddUserAtServerUseCase,
   val deleteServerUseCase: DeleteServerUseCase,
+  val updateHostAtServerUseCase: UpdateHostAtServerUseCase,
   val authClient: AuthClient,
 ) {
   @PostMapping("")
@@ -102,6 +101,17 @@ class RestApiServerController(
   ): ResponseEntity<Any> {
     val userInfo = authClient.getTokenInfo(token).data!!
     deleteServerUseCase.deleteServer(serverId, userInfo.userId)
+    return ResponseEntity.ok().build()
+  }
+
+  @PatchMapping("/{serverId}")
+  fun updateServerHost(
+    @PathVariable serverId: String,
+    @RequestBody otherUser: UpdateHostAtServerRequest,
+    @RequestHeader("Authorization") token: String,
+  ): ResponseEntity<Any> {
+    val userInfo = authClient.getTokenInfo(token).data!!
+    updateHostAtServerUseCase.updateServerHost(serverId, userInfo.userId, otherUser)
     return ResponseEntity.ok().build()
   }
 }

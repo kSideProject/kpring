60b29f243db95518320e9719d560c73498576957
minisun <minisundev@gmail.com>
2024-09-11 18:54:56 +0900

    feat: RoomChatRepository에서 pageable 삭제

--- chat/src/main/kotlin/kpring/chat/chat/repository/RoomChatRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chat/repository/RoomChatRepository.kt
@@ -1,15 +1,9 @@
 package kpring.chat.chat.repository
 
 import kpring.chat.chat.model.Chat
-import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
-import org.springframework.stereotype.Repository
 
-@Repository
 interface RoomChatRepository : MongoRepository<Chat, String>, QuerydslPredicateExecutor<Chat> {
-  fun findAllByContextId(
-    contextId: String,
-    pageable: Pageable,
-  ): List<Chat>
+  fun findAllByContextId(contextId: String): List<Chat>
 }

804aca49edfe71a1240bec7842520cc4f23d6f4a
minisun <minisundev@gmail.com>
2024-09-25 22:26:06 +0900

    refac: ServerChatRepository와 ChatRepository 통합

--- chat/src/main/kotlin/kpring/chat/chat/repository/ChatRepository.kt
+++ chat/src/main/kotlin/kpring/chat/chat/repository/ChatRepository.kt
@@ -4,4 +4,4 @@ import kpring.chat.chat.model.Chat
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
 
-interface RoomChatRepository : MongoRepository<Chat, String>, QuerydslPredicateExecutor<Chat>
+interface ChatRepository : MongoRepository<Chat, String>, QuerydslPredicateExecutor<Chat>

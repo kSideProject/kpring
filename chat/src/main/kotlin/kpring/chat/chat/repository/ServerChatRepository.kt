e7eefeec32cea83762d09e367e811028fa4a1e97
minisun <minisundev@gmail.com>
2024-09-25 22:24:16 +0900

    refac: ServerChat과 Chat 통합해서 ServerChatRepository 삭제

--- chat/src/main/kotlin/kpring/chat/chat/repository/ServerChatRepository.kt
+++
@@ -1,9 +0,0 @@
-package kpring.chat.chat.repository
-
-import kpring.chat.chat.model.Chat
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.data.querydsl.QuerydslPredicateExecutor
-
-interface ServerChatRepository : MongoRepository<Chat, String>, QuerydslPredicateExecutor<Chat> {
-  fun findAllByContextId(serverId: String): List<Chat>
-}

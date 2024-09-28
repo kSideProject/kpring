a7ff589d5bcebd249b42c3338dc864a370a4467c
minisun <minisundev@gmail.com>
2024-09-28 13:42:26 +0900

    refac: ChatRoomTest를 ContextTest로 변경하고 Server test 정보도 저장

--- chat/src/test/kotlin/kpring/chat/global/ChatRoomTest.kt
+++
@@ -1,8 +0,0 @@
-package kpring.chat.global
-
-interface ChatRoomTest {
-  companion object {
-    const val TEST_ROOM_ID = "6639d64f263b3c4e6f3a6982"
-    val TEST_MEMBERS = listOf("userid", "userid2")
-  }
-}
---
+++ chat/src/test/kotlin/kpring/chat/global/ContextTest.kt
@@ -0,0 +1,9 @@
+package kpring.chat.global
+
+interface ContextTest {
+  companion object {
+    const val TEST_ROOM_ID = "test_room_id"
+    val TEST_MEMBERS = listOf("userid", "userid2")
+    const val TEST_SERVER_ID = "test_server_id"
+  }
+}

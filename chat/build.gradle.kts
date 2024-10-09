4bac66b380ac56d4435882586bf63b2368d79d20
minisun <minisundev@gmail.com>
2024-10-09 15:53:21 +0900

    chore: websocket security dependency 추가

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -78,9 +78,11 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+  // websocket security
+  implementation("org.springframework.security:spring-security-messaging")
+
   // Web Security
   implementation("org.springframework.boot:spring-boot-starter-security")
-
 }
 
 kapt {

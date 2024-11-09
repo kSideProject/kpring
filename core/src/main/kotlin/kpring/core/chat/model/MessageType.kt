69d82452061870ff4615ea1bd2e31f0d0951ab18
minisun <minisundev@gmail.com>
2024-11-09 22:20:20 +0900

    feat: MessageType에서 채팅방에 해당하는 것 EventType으로 분리

--- core/src/main/kotlin/kpring/core/chat/model/MessageType.kt
+++ core/src/main/kotlin/kpring/core/chat/model/MessageType.kt
@@ -1,10 +1,7 @@
 package kpring.core.chat.model
 
 enum class MessageType(val type: String) {
-  ENTER("ENTER"),
-  INVITATION("INVITATION"),
   CHAT("CHAT"),
   UPDATE("UPDATE"),
   DELETE("DELETE"),
-  OUT("OUT"),
 }

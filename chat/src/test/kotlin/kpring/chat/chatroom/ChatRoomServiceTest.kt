fa52312d5306c7800110a8295a61885947378b17
minisun <minisundev@gmail.com>
2024-11-09 22:21:22 +0900

    feat: 테스트코드에 수정 사항 반영

--- chat/src/test/kotlin/kpring/chat/chatroom/ChatRoomServiceTest.kt
+++ chat/src/test/kotlin/kpring/chat/chatroom/ChatRoomServiceTest.kt
@@ -2,28 +2,36 @@ package kpring.chat.chatroom
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldNotContain
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
+import kpring.chat.chat.model.Chat
+import kpring.chat.chat.repository.ChatRepository
 import kpring.chat.chatroom.model.ChatRoom
+import kpring.chat.chatroom.model.EventType
 import kpring.chat.chatroom.repository.ChatRoomRepository
 import kpring.chat.chatroom.service.ChatRoomService
 import kpring.chat.chatroom.service.InvitationService
 import kpring.chat.global.CommonTest
 import kpring.chat.global.ContextTest
+import kpring.chat.global.util.AccessVerifier
 import kpring.core.chat.chatroom.dto.request.CreateChatRoomRequest
+import kpring.core.chat.model.ChatType
 import java.util.*
 
 class ChatRoomServiceTest : FunSpec({
 
   val chatRoomRepository = mockk<ChatRoomRepository>()
+  val chatRepository = mockk<ChatRepository>()
   val invitationService = mockk<InvitationService>()
-  val chatRoomService = ChatRoomService(chatRoomRepository, invitationService)
+  val accessVerifier = mockk<AccessVerifier>()
+
+  val chatRoomService = ChatRoomService(chatRoomRepository, chatRepository, invitationService, accessVerifier)
 
   test("createChatRoom 는 새 ChatRoom을 저장해야 한다") {
     // Given
     val request = CreateChatRoomRequest(ContextTest.TEST_MEMBERS)
-    val chatRoom = ChatRoom()
+    val chatRoom = ChatRoom("test_chatroom_id", mutableSetOf(CommonTest.TEST_USER_ID))
+    val chat = Chat("chat_id", CommonTest.TEST_USER_ID, ChatType.ROOM, EventType.CHAT, ContextTest.TEST_ROOM_ID, "content")
+    every { chatRepository.save(any()) } returns chat
     every { chatRoomRepository.save(any()) } returns chatRoom
 
     // When
@@ -45,6 +53,9 @@ class ChatRoomServiceTest : FunSpec({
     every { chatRoomRepository.findById(chatRoom.id!!) } returns Optional.of(chatRoom)
     every { chatRoomRepository.save(any()) } returns chatRoom
     every { chatRoomRepository.existsByIdAndMembersContaining(any(), CommonTest.TEST_USER_ID) } returns true
+    every { accessVerifier.verifyChatRoomAccess(any(), any()) } just runs
+    val chat = Chat("chat_id", CommonTest.TEST_USER_ID, ChatType.ROOM, EventType.CHAT, ContextTest.TEST_ROOM_ID, "content")
+    every { chatRepository.save(any()) } returns chat
 
     // When
     chatRoomService.exitChatRoom(chatRoom.id!!, CommonTest.TEST_USER_ID)

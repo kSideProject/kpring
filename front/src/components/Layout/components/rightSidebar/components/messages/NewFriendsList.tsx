be091f350d38546570a107a99fd9c548f586815e
jihyun-j <jihyun9142@gmail.com>
2024-12-06 11:13:48 +0900

    (test) 친구수락버튼 이벤트 추가

--- front/src/components/Layout/components/rightSidebar/components/messages/NewFriendsList.tsx
+++ front/src/components/Layout/components/rightSidebar/components/messages/NewFriendsList.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import useNewRequestedFriends from "../../hooks/useNewRequestedFriends";
 import Avatar from "boring-avatars";
+import axios from "axios";
 
 const NewFriendsList = () => {
   const token = localStorage.getItem("dicoTown_AccessToken");
   const data = useNewRequestedFriends("2", token);
+
+  const acceptRequestedFriend = async (
+    userId: string | null,
+    friendId: string | null,
+    token: string | null
+  ) => {
+    const url = `${process.env.REACT_APP_BASE_URL}/user/api/v1/user/${userId}/friend/${friendId}`;
+
+    try {
+      const response = await axios({
+        method: "patch",
+        url,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
@@ -17,7 +40,9 @@ const NewFriendsList = () => {
             <span>{newFriend.username}</span>
           </div>
           <div className="flex gap-2">
-            <button>수락</button>
+            <button onClick={() => acceptRequestedFriend("4", "2", token)}>
+              수락
+            </button>
             <button>거절</button>
           </div>
         </div>

4a2d8df13770e6fe159d8559370bc31444352eec
minisun <minisundev@gmail.com>
2024-12-09 22:27:38 +0900

    feat: 프론트 개발환경에서 접근 가능하게 cors 걸러줌

--- user/src/main/kotlin/kpring/user/config/SecurityConfig.kt
+++ user/src/main/kotlin/kpring/user/config/SecurityConfig.kt
@@ -2,13 +2,38 @@ package kpring.user.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
   @Bean
   fun encode(): PasswordEncoder {
     return BCryptPasswordEncoder()
+  }
+
+  @Bean
+  fun filterChain(http: HttpSecurity): SecurityFilterChain {
+    http
+      .cors { cors ->
+        cors.configurationSource {
+          val cors = CorsConfiguration()
+          cors.allowedOrigins = listOf("http://localhost:3000")
+          cors.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH")
+          cors.allowedHeaders = listOf("*")
+          cors.allowCredentials = true
+          cors
+        }
+      }
+      .csrf { it.disable() }
+      .formLogin { it.disable() }
+      .httpBasic { }
+
+    return http.build()
   }
 }

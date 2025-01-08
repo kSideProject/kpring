2f1ea86c53925493913106cd9f4c7e5110087eab
jihyun-j <jihyun9142@gmail.com>
2025-01-08 17:18:56 +0900

    [style] 오른쪽 메세지 사이드바 스타일 수정

--- front/src/components/molecules/SideBarHaeder.tsx
+++ front/src/components/molecules/SideBarHaeder.tsx
@@ -11,10 +11,14 @@ const SideBarHaeder: React.FC<SideBarHeaderProps> = ({
 }) => {
   return (
     <div className="flex justify-between items-center mb-5 max-h-12">
-      {activeSideBar === "friends" ? <p>Friends</p> : <p>Massages</p>}
+      {activeSideBar === "friends" ? (
+        <span className="text-h6 font-bold">Friends</span>
+      ) : (
+        <span className="text-h6 font-bold">Massages</span>
+      )}
       <IoCloseCircle
         onClick={onClose}
-        className="text-3xl text-white hover:text-red-500"
+        className="text-h4 text-black cursor-pointer transition duration-300 hover:text-red-500"
       />
     </div>
   );

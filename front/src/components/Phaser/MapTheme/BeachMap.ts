6c502db97781e953c8cf51a62c140e155c64f851
jihyun-j <jihyun9142@gmail.com>
2025-01-08 17:21:19 +0900

    [refector] 해변 테마 맵 아바타 닉네임 추가

--- front/src/components/Phaser/MapTheme/BeachMap.ts
+++ front/src/components/Phaser/MapTheme/BeachMap.ts
@@ -1,16 +1,27 @@
 import { Scene } from "phaser";
-import { createRandomAvatar, randomSkin } from "../Scenes/Avatar";
-import { controlAvatarAnimations } from "../Avatar/controlAvatar";
+import { createRandomAvatar } from "../Scenes/Avatar";
+import { setupCameraControls } from "../Scenes/cameraControls";
 
 export class BeachMap extends Scene {
   private avatar!: Phaser.GameObjects.Container;
   private keyboards!: Phaser.Types.Input.Keyboard.CursorKeys | null;
+  private spaceKey!: Phaser.Input.Keyboard.Key | undefined;
+  private isJumping: boolean = false;
+  private nickname: string;
 
-  constructor() {
+  constructor(nickname: string) {
     super("BeachMap");
+    this.nickname = nickname;
+  }
+
+  init(data: { nickname: string }) {
+    this.nickname = data.nickname;
   }
 
   create() {
+    this.spaceKey = this.input.keyboard?.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
     const beachMap = this.make.tilemap({ key: "beach" });
     const beachTilesets = beachMap.addTilesetImage(
       "beach_tilesets",
@@ -33,14 +44,20 @@ export class BeachMap extends Scene {
         faceColor: new Phaser.Display.Color(48, 38, 37, 255),
       });
 
-      this.avatar = createRandomAvatar(this, 550, 350);
+      this.avatar = createRandomAvatar(this, 550, 350, this.nickname);
+      this.cameras.main.startFollow(this.avatar);
+      this.cameras.main.setZoom(2);
+      this.avatar.setDepth(10);
+
       this.add.existing(this.avatar);
       this.avatar.setScale(2);
-      this.cameras.main.startFollow(this.avatar);
 
       if (bottomGroundLayer)
         this.physics.add.collider(this.avatar, bottomGroundLayer);
     }
+
+    setupCameraControls(this, this.avatar);
+    this.isJumping = false;
 
     if (this.input.keyboard) {
       this.keyboards = this.input.keyboard.createCursorKeys();
